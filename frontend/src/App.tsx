b631e1a84585a2a8814113e221cf28e0dea72c41
kimsanglok <evertree6031@gmail.com>
2022-07-01 21:58:20 +0900

    feat: 초기환경 설정
    resolved: #2

---
+++ frontend/src/App.tsx
@@ -0,0 +1,5 @@
+const App = () => {
+  return <div>Hello Hojin</div>;
+};
+
+export default App;
---
+++ frontend/src/index.tsx
@@ -0,0 +1,7 @@
+import { createRoot } from "react-dom/client";
+import App from "./App";
+
+const container = document.getElementById("root");
+const root = createRoot(container as Element);
+
+root.render(<App />);
