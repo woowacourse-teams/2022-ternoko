1d752f4cf5872cb59afad2ebd2c179ef0727048e
kimsanglok <evertree6031@gmail.com>
2022-07-04 21:52:21 +0900

    chore: styled-components 설치

--- frontend/config/webpack.common.js
+++ frontend/config/webpack.common.js
@@ -1,30 +1,27 @@
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const path = require("path");
-const webpack = require("webpack");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+const webpack = require('webpack');
 
 module.exports = {
-  entry: `${path.resolve(__dirname, "../src")}/index.tsx`,
+  entry: `${path.resolve(__dirname, '../src')}/index.tsx`,
   module: {
     rules: [
       {
         test: /\.(ts|tsx|js|jsx)$/,
-        use: "babel-loader",
+        use: 'babel-loader',
         exclude: /node_modules/,
       },
     ],
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: `${path.resolve(__dirname, "../public")}/index.html`,
+      template: `${path.resolve(__dirname, '../public')}/index.html`,
     }),
     new webpack.ProvidePlugin({
-      React: "react",
+      React: 'react',
     }),
   ],
   resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "..src/"),
-    },
-    extensions: [".js", ".ts", ".jsx", ".tsx", ".css", ".json"],
+    extensions: ['.js', '.ts', '.jsx', '.tsx', '.css', '.json'],
   },
 };
