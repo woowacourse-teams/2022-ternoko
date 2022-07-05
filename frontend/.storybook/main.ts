3f56bb592da408bd5dde9e86349124dc19df933a
kimsanglok <evertree6031@gmail.com>
2022-07-05 10:54:15 +0900

    feat: Header 컴포넌트 개발

--- frontend/.storybook/main.ts
+++ frontend/.storybook/main.ts
@@ -1,5 +1,9 @@
 module.exports = {
-  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  stories: [
+    '../src/**/*.stories.mdx',
+    '../src/**/*.stories.@(js|jsx|ts|tsx)',
+    '../src/**/*/*.stories.@(js|jsx|ts|tsx)',
+  ],
   addons: [
     '@storybook/addon-links',
     '@storybook/addon-essentials',
@@ -7,6 +11,6 @@ module.exports = {
   ],
   framework: '@storybook/react',
   core: {
-    builder: 'webpack5',
+    builder: '@storybook/builder-webpack5',
   },
 };
---
+++ frontend/src/components/Header/styled.ts
@@ -0,0 +1,26 @@
+import styled from 'styled-components';
+
+export const Box = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  height: 60px;
+  padding: 0 9rem;
+`;
+
+export const Logo = styled.div`
+  cursor: pointer;
+`;
+
+export const Menu = styled.div`
+  display: flex;
+  gap: 1rem;
+`;
+
+export const MenuItem = styled.p`
+  cursor: pointer;
+
+  &:hover {
+    text-decoration: underline;
+  }
+`;
