19bcebcef1c6ff29b340bb9c8e4d79a3917d2e40
kimsanglok <evertree6031@gmail.com>
2022-07-05 15:35:48 +0900

    style: Header 컴포넌트 스타일 수정

--- frontend/src/components/Header/index.tsx
+++ frontend/src/components/Header/index.tsx
@@ -3,11 +3,13 @@ import * as S from './styled';
 const Header = () => {
   return (
     <S.Box>
-      <S.Logo>터놓고</S.Logo>
-      <S.Menu>
+      <S.LogoBox>
+        <h1>터놓고</h1>
+      </S.LogoBox>
+      <S.MenuBox>
         <S.MenuItem>로그인</S.MenuItem>
         <S.MenuItem>회원가입</S.MenuItem>
-      </S.Menu>
+      </S.MenuBox>
     </S.Box>
   );
 };
