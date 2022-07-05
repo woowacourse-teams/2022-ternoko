03607f4e11673d239842f555a468e3dd020935bf
kimsanglok <evertree6031@gmail.com>
2022-07-05 11:42:36 +0900

    feat: Button 컴포넌트 개발

---
+++ frontend/src/components/@common/Button/Button.stories.tsx
@@ -0,0 +1,17 @@
+import { Story } from '@storybook/react';
+
+import { Button, ButtonProps } from './styled';
+
+export default {
+  title: 'components/@common/Button',
+  component: Button,
+};
+
+const Template = (args: ButtonProps) => <Button {...args}>안녕하세요</Button>;
+
+export const Basic: Story<ButtonProps> = Template.bind({});
+
+Basic.args = {
+  width: '100px',
+  height: '40px',
+};
