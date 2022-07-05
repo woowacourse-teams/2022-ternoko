0db028a3e1518ed9e2cd8d6928222b8957031722
kimsanglok <evertree6031@gmail.com>
2022-07-05 16:37:43 +0900

    feat: ReservationApplyPage - 코치 선택영역 개발

---
+++ frontend/src/pages/ReservationApplyPage/styled.tsx
@@ -0,0 +1,48 @@
+import styled from 'styled-components';
+
+export const Box = styled.div``;
+
+export const TitleBox = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 2rem;
+
+  h2 {
+    position: relative;
+    font-size: 3rem;
+    cursor: pointer;
+  }
+`;
+
+export const SubTitleBox = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 1rem;
+  margin-bottom: 2rem;
+
+  font-weight: bold;
+
+  h3 {
+    font-size: 1.3rem;
+  }
+`;
+
+export const Circle = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 20px;
+  height: 20px;
+  border-radius: 100%;
+
+  background-color: ${({ theme }) => theme.colors.pink};
+  color: ${({ theme }) => theme.colors.white};
+`;
+
+export const CoachProfileContainer = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(110px, 1fr));
+  grid-gap: 3rem;
+  padding-bottom: 3rem;
+`;
