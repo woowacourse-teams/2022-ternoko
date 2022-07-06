b0a1189e056b70b5a80221620ab3a3eedbd58c16
kimsanglok <evertree6031@gmail.com>
2022-07-06 11:35:57 +0900

    refactor: 중복되는 로직 단순화

--- frontend/src/pages/HomePage/styled.tsx
+++ frontend/src/pages/HomePage/styled.tsx
@@ -1,10 +1,5 @@
 import styled from 'styled-components';
 
-export const Box = styled.div`
-  width: 100%;
-  height: 100%;
-`;
-
 export const TitleBox = styled.div`
   display: flex;
   justify-content: space-between;
--- frontend/src/pages/ReservationApplyPage/index.tsx
+++ frontend/src/pages/ReservationApplyPage/index.tsx
@@ -3,39 +3,29 @@ import { useState } from 'react';
 import Button from '../../components/@common/Button/styled';
 import CoachProfile from '../../components/CoachProfile';
 import GridContainer from '../../components/@common/GridContainer/styled';
-import * as S from './styled';
 import TextAreaField from '../../components/TextAreaField';
+
+import * as S from './styled';
 
 export type StepStatus = 'show' | 'hidden' | 'onlyShowTitle';
 
 const ReservationApplyPage = () => {
-  const [step1Status, setStep1Status] = useState<StepStatus>('onlyShowTitle');
-  const [step2Status, setStep2Status] = useState<StepStatus>('onlyShowTitle');
-  const [step3Status, setStep3Status] = useState<StepStatus>('show');
+  const [stepStatus, setStepStatus] = useState<StepStatus[]>(['show', 'hidden', 'hidden']);
 
-  const handleClickStep1NextButton = () => {
-    setStep1Status('onlyShowTitle');
-    setStep2Status('show');
+  const handleClickStepTitle = (step: number) => {
+    setStepStatus((prevStepStatus) =>
+      prevStepStatus.map((stepStatus, index) =>
+        index === step ? 'show' : index > step ? 'hidden' : stepStatus,
+      ),
+    );
   };
 
-  const handleClickStep1Title = () => {
-    setStep1Status('show');
-    setStep2Status('hidden');
-    setStep3Status('hidden');
-  };
-
-  const handleClickStep2NextButton = () => {
-    setStep2Status('onlyShowTitle');
-    setStep3Status('show');
-  };
-
-  const handleClickStep2Title = () => {
-    setStep2Status('show');
-    setStep3Status('hidden');
-  };
-
-  const handleClickStep3NextButton = () => {
-    //api 호출
+  const handleClickStepNextButton = (step: number) => {
+    setStepStatus((prevStepStatus) =>
+      prevStepStatus.map((stepStatus, index) =>
+        index === step ? 'onlyShowTitle' : index === step + 1 ? 'show' : stepStatus,
+      ),
+    );
   };
 
   return (
@@ -44,8 +34,8 @@ const ReservationApplyPage = () => {
         <h2>{'<'} 면담 신청하기</h2>
       </S.TitleBox>
       <S.Container>
-        <S.Box stepStatus={step1Status}>
-          <div className="subTitle" onClick={handleClickStep1Title}>
+        <S.Box stepStatus={stepStatus[0]}>
+          <div className="subTitle" onClick={() => handleClickStepTitle(0)}>
             <S.Circle>1</S.Circle>
             <h3>코치를 선택해주세요.</h3>
           </div>
@@ -65,14 +55,14 @@ const ReservationApplyPage = () => {
               <CoachProfile />
             </GridContainer>
 
-            <Button width="100%" height="40px" onClick={handleClickStep1NextButton}>
+            <Button width="100%" height="40px" onClick={() => handleClickStepNextButton(0)}>
               다음
             </Button>
           </div>
         </S.Box>
 
-        <S.Box stepStatus={step2Status}>
-          <div className="subTitle" onClick={handleClickStep2Title}>
+        <S.Box stepStatus={stepStatus[1]}>
+          <div className="subTitle" onClick={() => handleClickStepTitle(1)}>
             <S.Circle>2</S.Circle>
             <h3>날짜 및 시간을 선택해주세요.</h3>
           </div>
@@ -82,13 +72,13 @@ const ReservationApplyPage = () => {
               <CoachProfile />
             </GridContainer>
 
-            <Button width="100%" height="40px" onClick={handleClickStep2NextButton}>
+            <Button width="100%" height="40px" onClick={() => handleClickStepNextButton(1)}>
               다음
             </Button>
           </div>
         </S.Box>
 
-        <S.Box stepStatus={step3Status}>
+        <S.Box stepStatus={stepStatus[2]}>
           <div className="subTitle">
             <S.Circle>3</S.Circle>
             <h3>사전 논의 내용을 입력해주세요.</h3>
