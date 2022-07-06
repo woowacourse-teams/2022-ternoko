d57efdd2af18a294f9c22b181c726a620a739a3e
kimsanglok <evertree6031@gmail.com>
2022-07-06 12:07:39 +0900

    feat: react-router-dom 적용

--- frontend/src/components/Header/index.tsx
+++ frontend/src/components/Header/index.tsx
@@ -1,11 +1,13 @@
+import { Link } from 'react-router-dom';
+
 import * as S from './styled';
 
 const Header = () => {
   return (
     <S.Box>
-      <S.LogoBox>
+      <Link to="/">
         <h1>터놓고</h1>
-      </S.LogoBox>
+      </Link>
       <S.MenuBox>
         <S.MenuItem>로그인</S.MenuItem>
         <S.MenuItem>회원가입</S.MenuItem>
