1d96be7931c9aeaa2c0bf708faffbaff175ef13c
kimsanglok <evertree6031@gmail.com>
2022-07-06 11:16:04 +0900

    feat: ReservationApplyPage - 사전 논의 내용 입력영역 개발

--- frontend/src/pages/ReservationApplyPage/index.tsx
+++ frontend/src/pages/ReservationApplyPage/index.tsx
@@ -1,36 +1,112 @@
+import { useState } from 'react';
+
 import Button from '../../components/@common/Button/styled';
 import CoachProfile from '../../components/CoachProfile';
 import GridContainer from '../../components/@common/GridContainer/styled';
 import * as S from './styled';
+import TextAreaField from '../../components/TextAreaField';
+
+export type StepStatus = 'show' | 'hidden' | 'onlyShowTitle';
 
 const ReservationApplyPage = () => {
+  const [step1Status, setStep1Status] = useState<StepStatus>('onlyShowTitle');
+  const [step2Status, setStep2Status] = useState<StepStatus>('onlyShowTitle');
+  const [step3Status, setStep3Status] = useState<StepStatus>('show');
+
+  const handleClickStep1NextButton = () => {
+    setStep1Status('onlyShowTitle');
+    setStep2Status('show');
+  };
+
+  const handleClickStep1Title = () => {
+    setStep1Status('show');
+    setStep2Status('hidden');
+    setStep3Status('hidden');
+  };
+
+  const handleClickStep2NextButton = () => {
+    setStep2Status('onlyShowTitle');
+    setStep3Status('show');
+  };
+
+  const handleClickStep2Title = () => {
+    setStep2Status('show');
+    setStep3Status('hidden');
+  };
+
+  const handleClickStep3NextButton = () => {
+    //api 호출
+  };
+
   return (
-    <S.Box>
+    <>
       <S.TitleBox>
         <h2>{'<'} 면담 신청하기</h2>
       </S.TitleBox>
-      <S.SubTitleBox>
-        <S.Circle>1</S.Circle>
-        <h3>코치를 선택해주세요.</h3>
-      </S.SubTitleBox>
+      <S.Container>
+        <S.Box stepStatus={step1Status}>
+          <div className="subTitle" onClick={handleClickStep1Title}>
+            <S.Circle>1</S.Circle>
+            <h3>코치를 선택해주세요.</h3>
+          </div>
 
-      <GridContainer minSize="110px" pb="3rem">
-        <CoachProfile />
-        <CoachProfile />
-        <CoachProfile />
-        <CoachProfile />
-        <CoachProfile />
-        <CoachProfile />
-        <CoachProfile />
-        <CoachProfile />
-        <CoachProfile />
-        <CoachProfile />
-      </GridContainer>
+          <div className="fold-box">
+            <GridContainer minSize="110px" pb="3rem">
+              <CoachProfile />
+              <CoachProfile />
+              <CoachProfile />
+              <CoachProfile />
+              <CoachProfile />
+              <CoachProfile />
+              <CoachProfile />
+              <CoachProfile />
+              <CoachProfile />
+              <CoachProfile />
+              <CoachProfile />
+            </GridContainer>
 
-      <Button width="100%" height="40px">
-        다음
-      </Button>
-    </S.Box>
+            <Button width="100%" height="40px" onClick={handleClickStep1NextButton}>
+              다음
+            </Button>
+          </div>
+        </S.Box>
+
+        <S.Box stepStatus={step2Status}>
+          <div className="subTitle" onClick={handleClickStep2Title}>
+            <S.Circle>2</S.Circle>
+            <h3>날짜 및 시간을 선택해주세요.</h3>
+          </div>
+
+          <div className="fold-box">
+            <GridContainer minSize="110px" pb="3rem">
+              <CoachProfile />
+            </GridContainer>
+
+            <Button width="100%" height="40px" onClick={handleClickStep2NextButton}>
+              다음
+            </Button>
+          </div>
+        </S.Box>
+
+        <S.Box stepStatus={step3Status}>
+          <div className="subTitle">
+            <S.Circle>3</S.Circle>
+            <h3>사전 논의 내용을 입력해주세요.</h3>
+          </div>
+
+          <div className="fold-box">
+            <S.Form>
+              <TextAreaField id="example1" label="이번 면담을 통해 논의하고 싶은 내용" />
+              <TextAreaField id="example2" label="최근에 자신이 긍정적으로 보는 시도와 변화" />
+              <TextAreaField id="example3" label="이번 면담을 통해 어떤 변화가 생기기를 원하는지" />
+              <Button width="100%" height="40px">
+                신청 완료
+              </Button>
+            </S.Form>
+          </div>
+        </S.Box>
+      </S.Container>
+    </>
   );
 };
 
--- frontend/src/pages/ReservationApplyPage/styled.tsx
+++ frontend/src/pages/ReservationApplyPage/styled.tsx
@@ -1,6 +1,63 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
+import { StepStatus } from '.';
 
-export const Box = styled.div``;
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 3rem;
+`;
+
+type BoxProps = {
+  stepStatus: StepStatus;
+};
+
+export const Box = styled.div<BoxProps>`
+  .subTitle {
+    display: flex;
+    align-items: center;
+    gap: 1rem;
+    margin-bottom: 2rem;
+
+    font-weight: bold;
+
+    h3 {
+      font-size: 1.3rem;
+    }
+  }
+
+  ${({ stepStatus }) =>
+    stepStatus === 'show' &&
+    css`
+      overflow: visible;
+      opacity: 1;
+      border: 3px solid yellow;
+
+      .fold-box {
+        max-height: 1000px;
+      }
+    `}
+
+  ${({ stepStatus }) =>
+    stepStatus === 'hidden' &&
+    css`
+      max-height: 0;
+      opacity: 0;
+      border: 3px solid yellow;
+    `}
+
+  ${({ stepStatus }) =>
+    stepStatus === 'onlyShowTitle' &&
+    css`
+      overflow: visible;
+      opacity: 1;
+      border: 3px solid yellow;
+
+      .fold-box {
+        max-height: 0;
+        overflow: hidden;
+      }
+    `}
+`;
 
 export const TitleBox = styled.div`
   display: flex;
@@ -38,4 +95,17 @@ export const Circle = styled.div`
 
   background-color: ${({ theme }) => theme.colors.pink};
   color: ${({ theme }) => theme.colors.white};
+`;
+
+export const Bar = styled.div`
+  width: 1px;
+  height: 40px;
+  margin-left: 10px;
+  background: ${({ theme }) => theme.colors.pink};
+`;
+
+export const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+  gap: 2.5rem;
 `;
