3e2660963e49f34ee3c50d40b33fb0dac52a9fbf
sanaandmomo <qajjang46@naver.com>
2022-07-13 17:02:52 +0900

    commit test

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,7 +4,7 @@ import AppRoutes from './AppRoutes';
 
 import GlobalStyle from './styles/GlobalStyle';
 import theme from './styles/theme';
-
+//test
 const App = () => {
   return (
     <ThemeProvider theme={theme}>
