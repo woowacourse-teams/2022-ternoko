e01b4b1028dda695b8f1d6018478788241ecd342
dongho <dongho108@naver.com>
2022-07-14 19:16:53 +0900

    refactor: 현재 연도와 달 기준으로 year, month 파라미터 지정

--- backend/src/test/java/com/woowacourse/ternoko/acceptance/CoachAcceptanceTest.java
+++ backend/src/test/java/com/woowacourse/ternoko/acceptance/CoachAcceptanceTest.java
@@ -1,15 +1,18 @@
 package com.woowacourse.ternoko.acceptance;
 
-import static com.woowacourse.ternoko.fixture.MemberFixture.COACH4;
-import static org.assertj.core.api.Assertions.assertThat;
+import static com.woowacourse.ternoko.fixture.MemberFixture.*;
+import static com.woowacourse.ternoko.fixture.ReservationFixture.*;
+import static org.assertj.core.api.Assertions.*;
 
-import com.woowacourse.ternoko.dto.ScheduleResponse;
-import io.restassured.RestAssured;
-import io.restassured.response.ExtractableResponse;
-import io.restassured.response.Response;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.MediaType;
+
+import com.woowacourse.ternoko.dto.ScheduleResponse;
+
+import io.restassured.RestAssured;
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
 
 public class CoachAcceptanceTest extends AcceptanceTest {
 
@@ -24,8 +27,8 @@ public class CoachAcceptanceTest extends AcceptanceTest {
 
         // when
         final ExtractableResponse<Response> response = RestAssured.given().log().all()
-                .queryParam("year", 2022)
-                .queryParam("month", 7)
+                .queryParam("year", AFTER_TWO_DAYS.getYear())
+                .queryParam("month", AFTER_TWO_DAYS.getMonthValue())
                 .contentType(MediaType.APPLICATION_JSON_VALUE)
                 .when().get("/api/coaches/" + COACH4.getId() + "/schedules")
                 .then().log().all()
