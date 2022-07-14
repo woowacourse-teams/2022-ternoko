35e86b286a435266af2a1fb46ca470d98d243533
her0807 <her0807@naver.com>
2022-07-14 14:48:55 +0900

    fix: 누락된 CalendarResponse 추가 및 conflict 해결

---
+++ backend/src/main/java/com/woowacourse/ternoko/dto/CalendarResponse.java
@@ -0,0 +1,34 @@
+package com.woowacourse.ternoko.dto;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.woowacourse.ternoko.domain.Interview;
+import java.time.LocalDateTime;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder(builderMethodName = "calenderResponseBuilder")
+public class CalendarResponse {
+
+    private Long id;
+    private String crewNickname;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm", timezone = "Asia/Seoul")
+    private LocalDateTime interviewStartTime;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm", timezone = "Asia/Seoul")
+    private LocalDateTime interviewEndTime;
+
+    public static CalendarResponse from(final Interview interview) {
+        return CalendarResponse.calenderResponseBuilder()
+                .id(interview.getId())
+                .crewNickname(interview.getCrewNickname())
+                .interviewStartTime(
+                        interview.getInterviewStartTime())
+                .interviewEndTime(
+                        interview.getInterviewEndTime())
+                .build();
+    }
+}
--- backend/src/test/java/com/woowacourse/ternoko/fixture/ReservationFixture.java
+++ backend/src/test/java/com/woowacourse/ternoko/fixture/ReservationFixture.java
@@ -1,7 +1,5 @@
 package com.woowacourse.ternoko.fixture;
 
-import com.woowacourse.ternoko.domain.Member;
-import com.woowacourse.ternoko.domain.Type;
 import com.woowacourse.ternoko.dto.FormItemDto;
 import com.woowacourse.ternoko.dto.ReservationRequest;
 import java.time.LocalDateTime;
@@ -11,9 +9,9 @@ public class ReservationFixture {
 
     public static final Long INTERVIEW_TIME = 30L;
 
-    public static final List<FormItemRequest> FORM_ITEM_REQUESTS = List.of(new FormItemRequest("고정질문1", "답변1"),
-            new FormItemRequest("고정질문2", "답변2"),
-            new FormItemRequest("고정질문3", "답변3"));
+    public static final List<FormItemDto> FORM_ITEM_REQUESTS = List.of(new FormItemDto("고정질문1", "답변1"),
+            new FormItemDto("고정질문2", "답변2"),
+            new FormItemDto("고정질문3", "답변3"));
 
     public static final ReservationRequest RESERVATION_REQUEST1 = new ReservationRequest("바니",
             LocalDateTime.of(2022, 7, 4, 14, 0, 0),
