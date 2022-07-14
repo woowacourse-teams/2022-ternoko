b3f84500f7d4948083ad080410f6b3f309604541
sanaandmomo <qajjang46@naver.com>
2022-07-14 13:53:42 +0900

    feat: TitleBox 공통 컴포넌트로 분리
    Co-authored-by: Kimsanglok <evertree6031@gmail.com>

---
+++ frontend/src/components/@common/TitleBox/styled.tsx
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+export const TitleBox = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 2rem;
+
+  a {
+    margin-right: 1rem;
+  }
+
+  h2 {
+    position: relative;
+    font-size: 3rem;
+  }
+`;
