0b04caf5b6bad0524a2d9b9594ff11213f28ecad
sanaandmomo <qajjang46@naver.com>
2022-07-20 19:39:26 +0900

    feat: Header 로고 추가
    Co-authored-by: Kimsanglok <evertree6031@gmail.com>

--- frontend/src/components/Header/index.tsx
+++ frontend/src/components/Header/index.tsx
@@ -2,6 +2,9 @@ import { Link, useLocation } from 'react-router-dom';
 
 import * as S from './styled';
 
+const logoURL =
+  'https://www.notion.so/image/https%3A%2F%2Fs3-us-west-2.amazonaws.com%2Fsecure.notion-static.com%2F6b9fa6f5-4448-4033-97da-1c425b75bd35%2FIMG_92500CCD3DA4-1_2.jpeg?table=block&id=b24abb05-6f3f-4261-a429-50952525f7fb&spaceId=69b38b44-10b7-40af-9192-e032c5471fd2&width=2000&userId=c6e4abc8-389f-4426-a98c-49603c6569cb&cache=v2';
+
 const Header = () => {
   const location = useLocation();
 
@@ -9,10 +12,12 @@ const Header = () => {
     <S.Box>
       {location.pathname.includes('coach') ? (
         <Link to="/coach/home">
+          <img src={logoURL} />
           <h1>코치도 터놓고</h1>
         </Link>
       ) : (
         <Link to="/">
+          <img src={logoURL} />
           <h1>크루도 터놓고</h1>
         </Link>
       )}
