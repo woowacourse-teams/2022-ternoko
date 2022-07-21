97e1b6096543d362ab6fc511efc8a75aa4a85fc1
Kimsanglok <evertree6031@gmail.com>
2022-07-21 16:49:22 +0900

    [FE] fix: 배포할때 발생하는 에러 해결 (#90)
    * 머지 테스트
    
    * feat: 코치 면담 생성 페이지 개발
    
    * fix: 변경된 api 스펙에 맞게 수정
    
    Co-authored-by: Kimsanglok <evertree6031@gmail.com>
    
    * feat: Calendar 코치 스케줄 동기화
    
    Co-authored-by: Kimsanglok <evertree6031@gmail.com>
    
    * fix: 서버 url 변경
    
    Co-authored-by: soobin <sanaandmomo@users.noreply.github.com>
    
    * fix: Header 로고 url 변경
    
    * fix: webpack 실 서비스 output publicpath 수정
    
    Co-authored-by: sanaandmomo <qajjang46@naver.com>
    Co-authored-by: soobin <sanaandmomo@users.noreply.github.com>

--- frontend/config/webpack.prod.js
+++ frontend/config/webpack.prod.js
@@ -1,25 +1,25 @@
-const { merge } = require("webpack-merge");
-const common = require("./webpack.common");
-const path = require("path");
+const { merge } = require('webpack-merge');
+const common = require('./webpack.common');
+const path = require('path');
 
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const TerserPlugin = require("terser-webpack-plugin");
-const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 
 module.exports = merge(common, {
-  mode: "production",
-  devtool: "cheap-module-source-map",
+  mode: 'production',
+  devtool: 'cheap-module-source-map',
   output: {
-    filename: "[name].[contenthash].js",
-    path: path.resolve(__dirname, "../dist"),
-    publicPath: "./",
+    filename: '[name].[contenthash].js',
+    path: path.resolve(__dirname, '../dist'),
+    publicPath: '/',
     clean: true,
   },
   module: {
     rules: [
       {
         test: /\.(sa|sc|c)ss$/i,
-        use: [MiniCssExtractPlugin.loader, "css-loader"],
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
       },
     ],
   },
@@ -38,7 +38,7 @@ module.exports = merge(common, {
       new CssMinimizerPlugin(),
     ],
     splitChunks: {
-      chunks: "all",
+      chunks: 'all',
     },
   },
   performance: {
