9e7cfd6414d8edb0e2d68173354ef62ef09b5b66
Soomin Sohn <soomin.sohn.dev@gmail.com>
2022-07-26 16:48:30 +0900

    [BE] feat: 2차 스프린트까지의 기능 restdocs 적용 (#88)
    * feat: restdocs 적용
    
    * chore: build.gradle 미사용 의존성 삭제

---
+++ backend/src/test/java/com/woowacourse/ternoko/api/CoachControllerTest.java
@@ -0,0 +1,56 @@
+package com.woowacourse.ternoko.api;
+
+import static com.woowacourse.ternoko.fixture.MemberFixture.COACH1;
+import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
+import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.http.MediaType;
+import org.springframework.restdocs.payload.JsonFieldType;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+public class CoachControllerTest extends RestDocsTestSupport {
+
+    @Test
+    @DisplayName("코치 - 면담 예약 내역 목록을 조회한다.")
+    void findAllByCoach() throws Exception {
+        // given
+        createReservations(COACH1.getId());
+
+        // when, then
+        mockMvc.perform(MockMvcRequestBuilders
+                        .get("/api/coaches/" + COACH1.getId() + "/schedules")
+                        .queryParam("year", "2030")
+                        .queryParam("month", "8"))
+                .andExpect(status().isOk())
+                .andDo(restDocs.document(
+                        responseFields(
+                                fieldWithPath("calendar[]").type(JsonFieldType.ARRAY).description("코치 면담 예약 리스트"),
+                                fieldWithPath("calendar[].id").type(JsonFieldType.NUMBER).description("면담 예약 아이디"),
+                                fieldWithPath("calendar[].crewNickname").type(JsonFieldType.STRING)
+                                        .description("면담 예약한 크루 닉네임"),
+                                fieldWithPath("calendar[].interviewStartTime").type(JsonFieldType.STRING)
+                                        .description("면담 시작 시간"),
+                                fieldWithPath("calendar[].interviewEndTime").type(JsonFieldType.STRING)
+                                        .description("면담 종료 시간")
+                        )));
+    }
+
+    private void createReservations(final Long coachId) throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders
+                        .post("/api/reservations/coaches/{coachId}", coachId)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .characterEncoding("utf-8")
+                        .content(readJson("/json/reservations/create-reservation1.json")))
+                .andExpect(status().isCreated());
+
+        mockMvc.perform(MockMvcRequestBuilders
+                        .post("/api/reservations/coaches/{coachId}", coachId)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .characterEncoding("utf-8")
+                        .content(readJson("/json/reservations/create-reservation2.json")))
+                .andExpect(status().isCreated());
+    }
+}
---
+++ backend/src/test/java/com/woowacourse/ternoko/api/ReservationControllerTest.java
@@ -0,0 +1,96 @@
+package com.woowacourse.ternoko.api;
+
+import static com.woowacourse.ternoko.fixture.MemberFixture.COACH1;
+import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
+import static org.springframework.restdocs.payload.PayloadDocumentation.requestFields;
+import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.http.MediaType;
+import org.springframework.restdocs.payload.JsonFieldType;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+public class ReservationControllerTest extends RestDocsTestSupport {
+
+    @Test
+    @DisplayName("면담 예약을 생성한다.")
+    void create() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders
+                        .post("/api/reservations/coaches/{coachId}", COACH1.getId())
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .characterEncoding("utf-8")
+                        .content(readJson("/json/reservations/create-reservation1.json")))
+                .andExpect(status().isCreated())
+                .andDo(restDocs.document(
+                        requestFields(
+                                fieldWithPath("crewNickname").type(JsonFieldType.STRING).description("크루 닉네임"),
+                                fieldWithPath("interviewDatetime").type(JsonFieldType.STRING)
+                                        .description("인터뷰 시간"),
+                                fieldWithPath("interviewQuestions.[].question").description("면담질문1"),
+                                fieldWithPath("interviewQuestions.[].answer").description("면담답변1")
+                        )));
+    }
+
+    @Test
+    @DisplayName("면담 예약 상세 내역을 조회한다.")
+    void findReservationById() throws Exception {
+        // given
+        createReservation(COACH1.getId());
+
+        // when, then
+        mockMvc.perform(MockMvcRequestBuilders
+                        .get("/api/reservations/{reservationId}", 1))
+                .andExpect(status().isOk())
+                .andDo(restDocs.document(
+                        responseFields(
+                                fieldWithPath("id").type(JsonFieldType.NUMBER).description("예약 아이디"),
+                                fieldWithPath("coachNickname").type(JsonFieldType.STRING).description("코치 닉네임"),
+                                fieldWithPath("imageUrl").type(JsonFieldType.STRING).description("코치 이미지url"),
+                                fieldWithPath("crewNickname").type(JsonFieldType.STRING).description("크루 닉네임"),
+                                fieldWithPath("interviewStartTime").type(JsonFieldType.STRING).description("면담 시작 시간"),
+                                fieldWithPath("interviewEndTime").type(JsonFieldType.STRING).description("면담 종료 시간"),
+                                fieldWithPath("interviewQuestions[].question").type(JsonFieldType.STRING)
+                                        .description("면담 질문"),
+                                fieldWithPath("interviewQuestions[].answer").type(JsonFieldType.STRING)
+                                        .description("면담 내용")
+                        )));
+    }
+
+    @Test
+    @DisplayName("면담 예약 상세 내역 목록을 조회한다.")
+    void findAll() throws Exception {
+        // given
+        createReservation(COACH1.getId());
+
+        // when, then
+        mockMvc.perform(MockMvcRequestBuilders
+                        .get("/api/reservations"))
+                .andExpect(status().isOk())
+                .andDo(restDocs.document(
+                        responseFields( //response parameter
+                                fieldWithPath("[].id").type(JsonFieldType.NUMBER).description("예약 아이디"),
+                                fieldWithPath("[].coachNickname").type(JsonFieldType.STRING).description("코치 닉네임"),
+                                fieldWithPath("[].imageUrl").type(JsonFieldType.STRING).description("코치 이미지url"),
+                                fieldWithPath("[].crewNickname").type(JsonFieldType.STRING).description("크루 닉네임"),
+                                fieldWithPath("[].interviewStartTime").type(JsonFieldType.STRING)
+                                        .description("면담 시작 시간"),
+                                fieldWithPath("[].interviewEndTime").type(JsonFieldType.STRING).description("면담 종료 시간"),
+                                fieldWithPath("[].interviewQuestions").type(JsonFieldType.ARRAY).description("면담 내용"),
+                                fieldWithPath("[].interviewQuestions[].question").type(JsonFieldType.STRING)
+                                        .description("면담 질문"),
+                                fieldWithPath("[].interviewQuestions[].answer").type(JsonFieldType.STRING)
+                                        .description("면담 내용")
+                        )));
+    }
+
+    private void createReservation(final Long coachId) throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders
+                        .post("/api/reservations/coaches/{coachId}", coachId)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .characterEncoding("utf-8")
+                        .content(readJson("/json/reservations/create-reservation1.json")))
+                .andExpect(status().isCreated());
+    }
+}
