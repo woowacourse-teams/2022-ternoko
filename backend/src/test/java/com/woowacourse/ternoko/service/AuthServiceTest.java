5ce2581009403378465defba0cd37c57fd7c287c
her0807 <her0807@naver.com>
2022-07-26 17:02:17 +0900

    test: auth 코치, 크루 회원 가입 로직 테스트 추가

--- backend/src/test/java/com/woowacourse/ternoko/service/AuthServiceTest.java
+++ backend/src/test/java/com/woowacourse/ternoko/service/AuthServiceTest.java
@@ -4,8 +4,18 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
 
+import com.slack.api.methods.SlackApiException;
+import com.slack.api.methods.impl.MethodsClientImpl;
+import com.slack.api.methods.request.openid.connect.OpenIDConnectTokenRequest;
+import com.slack.api.methods.request.openid.connect.OpenIDConnectUserInfoRequest;
+import com.slack.api.methods.response.openid.connect.OpenIDConnectTokenResponse;
+import com.slack.api.methods.response.openid.connect.OpenIDConnectUserInfoResponse;
+import com.woowacourse.ternoko.common.JwtProvider;
+import com.woowacourse.ternoko.domain.member.Member;
+import com.woowacourse.ternoko.repository.CoachRepository;
+import com.woowacourse.ternoko.repository.CrewRepository;
+import com.woowacourse.ternoko.repository.MemberRepository;
 import java.io.IOException;
-
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -16,57 +26,77 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.test.mock.mockito.SpyBean;
 import org.springframework.test.context.ActiveProfiles;
 
-import com.slack.api.methods.SlackApiException;
-import com.slack.api.methods.impl.MethodsClientImpl;
-import com.slack.api.methods.request.openid.connect.OpenIDConnectTokenRequest;
-import com.slack.api.methods.request.openid.connect.OpenIDConnectUserInfoRequest;
-import com.slack.api.methods.response.openid.connect.OpenIDConnectTokenResponse;
-import com.slack.api.methods.response.openid.connect.OpenIDConnectUserInfoResponse;
-import com.woowacourse.ternoko.common.JwtProvider;
-import com.woowacourse.ternoko.repository.CoachRepository;
-import com.woowacourse.ternoko.repository.CrewRepository;
-import com.woowacourse.ternoko.repository.MemberRepository;
-
 @ExtendWith(MockitoExtension.class)
 @SpringBootTest
 @ActiveProfiles("test")
 public class AuthServiceTest {
 
-	@Autowired
-	private AuthService authService;
-	@MockBean
-	MethodsClientImpl slackMethodClient;
-	@Autowired
-	MemberRepository memberRepository;
-	@Autowired
-	CoachRepository coachRepository;
-	@Autowired
-	CrewRepository crewRepository;
-	@SpyBean
-	JwtProvider jwtProvider;
+    @Autowired
+    private AuthService authService;
+    @MockBean
+    MethodsClientImpl slackMethodClient;
+    @Autowired
+    MemberRepository memberRepository;
+    @Autowired
+    CoachRepository coachRepository;
+    @Autowired
+    CrewRepository crewRepository;
+    @SpyBean
+    JwtProvider jwtProvider;
 
-	@Test
-	@DisplayName("로그인을 하면 AccessToken을 발급한다.")
-	void login() throws SlackApiException, IOException {
-		// given
-		String code = "temp_code";
+    @Test
+    @DisplayName("로그인을 하면 AccessToken을 발급한다.")
+    void login() throws SlackApiException, IOException {
+        // given
+        setSlackMockData("sudal@gmail.com");
+        // when
+        String accessToken = authService.login("temp_code");
+        // then
+        assertThat(accessToken).isNotNull();
+    }
 
-		OpenIDConnectTokenResponse openIDConnectTokenResponse = new OpenIDConnectTokenResponse();
-		openIDConnectTokenResponse.setAccessToken("temp_accessToken");
+    @DisplayName("코치가 최초 로그인 시도시, 코치로 회원가입이 된다.")
+    @Test
+    void signup_coach() throws SlackApiException, IOException {
+        // given
+        final String coachEmail = "pobi@woowahan.com";
+        setSlackMockData(coachEmail);
+        // when
+        authService.login("temp_code");
+        final Member coach = memberRepository.findByEmail(coachEmail).get();
+        // then
+        assertThat(coachRepository.findById(coach.getId()).isPresent()).isTrue();
+    }
 
-		final OpenIDConnectUserInfoResponse userInfoResponse = new OpenIDConnectUserInfoResponse();
-		userInfoResponse.setEmail("sudal@gmail.com");
-		userInfoResponse.setName("sudal");
-		userInfoResponse.setTeamImage230("testImage");
+    @DisplayName("코치가 최초 로그인 시도시, 코치로 회원가입이 된다.")
+    @Test
+    void signup_crew() throws SlackApiException, IOException {
+        // given
+        final String crewEmail = "sudal@naver.com";
+        setSlackMockData(crewEmail);
+        // when
+        authService.login("temp_code");
+        final Member crew = memberRepository.findByEmail(crewEmail).get();
+        // then
+        assertThat(crewRepository.findById(crew.getId()).isPresent()).isTrue();
+    }
 
-		given(slackMethodClient.openIDConnectToken(any(OpenIDConnectTokenRequest.class)))
-			.willReturn(openIDConnectTokenResponse);
-		given(slackMethodClient.openIDConnectUserInfo(any(OpenIDConnectUserInfoRequest.class)))
-			.willReturn(userInfoResponse);
-		given(jwtProvider.createToken("1")).willReturn("temp_access_token");
-		// when
-		String accessToken = authService.login(code);
-		// then
-		assertThat(accessToken).isNotNull();
-	}
+
+    private void setSlackMockData(String userEmail) throws IOException, SlackApiException {
+
+        OpenIDConnectTokenResponse openIDConnectTokenResponse = new OpenIDConnectTokenResponse();
+        openIDConnectTokenResponse.setAccessToken("temp_accessToken");
+
+        final OpenIDConnectUserInfoResponse userInfoResponse = new OpenIDConnectUserInfoResponse();
+        userInfoResponse.setEmail(userEmail);
+        userInfoResponse.setName("sudal");
+        userInfoResponse.setTeamImage230("testImage");
+
+        given(slackMethodClient.openIDConnectToken(any(OpenIDConnectTokenRequest.class)))
+                .willReturn(openIDConnectTokenResponse);
+        given(slackMethodClient.openIDConnectUserInfo(any(OpenIDConnectUserInfoRequest.class)))
+                .willReturn(userInfoResponse);
+        given(jwtProvider.createToken("1")).willReturn("temp_access_token");
+    }
+
 }
