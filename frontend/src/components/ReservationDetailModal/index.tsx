3ef873824d131a817083346a72edd4f43da72b6a
kimsanglok <evertree6031@gmail.com>
2022-07-27 20:51:00 +0900

    ReservationDetailModal 컴포넌트 개발

--- frontend/src/components/ReservationDetailModal/index.tsx
+++ frontend/src/components/ReservationDetailModal/index.tsx
@@ -1,14 +1,16 @@
 import ReactDOM from 'react-dom';
-
 import * as S from './styled';
+
+import Accordion from '../../components/@common/Accordion';
 
 const deleteIconURL =
   'https://icons-for-free.com/download-icon-delete+remove+trash+trash+bin+trash+can+icon-1320073117929397588_512.png';
+
 const closeIconURL = 'https://icons-for-free.com/download-icon-CLOSE-131994911256789607_512.png';
 const calendarIconURL = 'https://cdn-icons-png.flaticon.com/512/7088/7088625.png';
 const timeIconURL = 'https://cdn-icons-png.flaticon.com/512/88/88291.png';
 const profileImageURL =
-  'https://www.tfmedia.co.kr/data/photos/20210730/art_1627367323247_0d08c3.jpg';
+  'https://user-images.githubusercontent.com/19251499/181178299-d5c7c4c3-249e-47a5-83fe-99c148c3d84b.jpg';
 
 const ReservationDetailModal = () => {
   return ReactDOM.createPortal(
@@ -16,11 +18,11 @@ const ReservationDetailModal = () => {
       <S.Frame>
         <S.IconContainer>
           <S.Icon src={deleteIconURL} alt="삭제 아이콘" active />
-          <S.Icon src={closeIconURL} alt="모달 창 닫기 아이콘" active />
+          <S.Icon src={closeIconURL} alt="모달 창 닫기 아이콘" active agg />
         </S.IconContainer>
         <S.Profile>
           <img src={profileImageURL} alt="프로필" />
-          <p>바니(사현빈)</p>
+          <p>수달(허수진)</p>
         </S.Profile>
         <S.InfoContainer>
           <S.Info>
@@ -36,6 +38,20 @@ const ReservationDetailModal = () => {
             <p>13:00 ~ 13:30</p>
           </S.Info>
         </S.InfoContainer>
+        <S.AccordionContainer>
+          <Accordion
+            title="이번 면담을 통해 논의하고 싶은 내용"
+            description="한 모금에 너를 깨워 심장은 뛰고 온몸으로 퍼져가지 단 한 번에 perfect caffeine, take it away 중독된 것 같은 느낌 lovin"
+          />
+          <Accordion
+            title="최근에 자신이 긍정적으로 보는 시도와 변화"
+            description="One shot, twenty-four seven 널 미치게 하는 이 색은 black What you waiting for? look how beautiful 넘치게 넘치게 가득 담아줄게"
+          />
+          <Accordion
+            title="이번 면담을 통해 어떤 변화가 생기기를 원하는지"
+            description="더 짙어진 나의 color 더 깊어진 bitter flavor 내 맘은 너만 느낄 수 있어 네 심장을 뛰게 할 나"
+          />
+        </S.AccordionContainer>
       </S.Frame>
     </S.Dimmer>,
     document.getElementById('modal') as HTMLElement,
--- frontend/src/components/ReservationDetailModal/styled.ts
+++ frontend/src/components/ReservationDetailModal/styled.ts
@@ -1,4 +1,4 @@
-import styled, { css } from 'styled-components';
+import styled, { css, keyframes } from 'styled-components';
 
 export const Dimmer = styled.div`
   position: fixed;
@@ -16,7 +16,7 @@ export const Dimmer = styled.div`
 export const Frame = styled.div`
   position: relative;
   z-index: 1;
-  padding: 2rem 3rem;
+  padding: 3rem 3rem;
 
   font-size: 1.5rem;
   background-color: ${({ theme }) => theme.colors.white_100};
@@ -24,16 +24,26 @@ export const Frame = styled.div`
 
 export const IconContainer = styled.div`
   position: absolute;
-  right: 0;
-  top: 0;
+  right: 3rem;
+  top: 2rem;
   display: flex;
-  gap: 0.5rem;
-  padding: 0.5rem 0.5rem 0 0;
+  gap: 1rem;
 `;
 
 type IconProps = {
   active?: boolean;
+  agg?: boolean;
 };
+
+const rotating = keyframes`
+  from {
+    transform: rotate(0deg);
+  }
+
+  to {
+    transform: rotate(360deg);
+  }
+`;
 
 export const Icon = styled.img<IconProps>`
   width: 2rem;
@@ -43,8 +53,18 @@ export const Icon = styled.img<IconProps>`
   ${({ active }) =>
     active &&
     css`
+      width: 2.5rem;
+      height: 2.5rem;
       cursor: pointer;
     `};
+
+  ${({ agg }) =>
+    agg &&
+    css`
+      :hover {
+        animation: ${rotating} 0.01s infinite;
+      }
+    `}
 `;
 
 export const Profile = styled.div`
@@ -53,9 +73,13 @@ export const Profile = styled.div`
   gap: 2rem;
   margin-bottom: 3rem;
 
+  font-size: 2rem;
+  font-weight: 600;
+
   img {
-    width: 70px;
-    height: 70px;
+    width: 8rem;
+    height: 8rem;
+
     object-fit: cover;
     border-radius: 100%;
   }
@@ -64,20 +88,29 @@ export const Profile = styled.div`
 export const InfoContainer = styled.div`
   display: flex;
   flex-direction: column;
-  gap: 1rem;
+  gap: 1.5rem;
 `;
 
 export const Info = styled.div`
   display: flex;
-  align-items: flex-start;
+  align-items: center;
+  font-size: 1.6rem;
 `;
 
 export const IconBox = styled.div`
   width: 4.5rem;
 
   img {
-    width: 1.5rem;
-    height: 1.5rem;
+    width: 3rem;
+    height: 3rem;
     object-fit: cover;
   }
+`;
+
+export const AccordionContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+
+  margin-top: 2rem;
 `;
