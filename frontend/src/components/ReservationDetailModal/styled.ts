800f6f6968ba29a9c83ac71d997d6cb2e62e4ac4
kimsanglok <evertree6031@gmail.com>
2022-07-28 14:22:26 +0900

    style: 모달 애니메이션 추가

--- frontend/src/components/ReservationDetailModal/styled.ts
+++ frontend/src/components/ReservationDetailModal/styled.ts
@@ -1,9 +1,13 @@
 import styled, { css, keyframes } from 'styled-components';
 
-export const Dimmer = styled.div`
+type ModalProps = {
+  open?: boolean;
+};
+
+export const Dimmer = styled.div<ModalProps>`
   position: fixed;
   left: 0;
-  top: 0;
+  top: -100%;
   display: flex;
   align-items: center;
   justify-content: center;
@@ -11,16 +15,17 @@ export const Dimmer = styled.div`
   height: 100%;
 
   background-color: ${({ theme }) => theme.colors.black_50};
-`;
+  opacity: 0;
+  transition: opacity 0.5s top 0.5s;
 
-export const Frame = styled.div`
-  position: relative;
-  z-index: 1;
-  padding: 3rem 3rem;
+  ${({ open }) =>
+    open &&
+    css`
+      top: 0;
+      transition: top 0.5s;
 
-  font-size: 1.5rem;
-  background-color: ${({ theme }) => theme.colors.white_100};
-  border-radius: 10px;
+      opacity: 1;
+    `};
 `;
 
 export const IconContainer = styled.div`
@@ -29,6 +34,8 @@ export const IconContainer = styled.div`
   top: 2rem;
   display: flex;
   gap: 1rem;
+
+  opacity: 0;
 `;
 
 type IconProps = {
@@ -76,12 +83,13 @@ export const Profile = styled.div`
 
   font-size: 2rem;
   font-weight: 600;
+  opacity: 0;
 
   img {
     width: 8rem;
     height: 8rem;
-
     object-fit: cover;
+
     border-radius: 100%;
   }
 `;
@@ -90,11 +98,13 @@ export const InfoContainer = styled.div`
   display: flex;
   flex-direction: column;
   gap: 1.5rem;
+  opacity: 0;
 `;
 
 export const Info = styled.div`
   display: flex;
   align-items: center;
+
   font-size: 1.6rem;
 `;
 
@@ -112,6 +122,54 @@ export const AccordionContainer = styled.div`
   display: flex;
   flex-direction: column;
   gap: 1rem;
-
   margin-top: 2rem;
+
+  opacity: 0;
+`;
+
+export const Frame = styled.div<ModalProps>`
+  position: relative;
+  top: -200%;
+  padding: 3rem 3rem;
+  transform: scale(0.5);
+  transition: opacity 0.5s ease-in-out, top 1s ease-in-out, transform 1s ease-in-out;
+
+  opacity: 0;
+  background-color: ${({ theme }) => theme.colors.white_100};
+  border-radius: 10px;
+  font-size: 1.5rem;
+
+  ${({ open }) =>
+    open &&
+    css`
+      top: 0;
+      transform: scale(1);
+      transition: transform 0.2s cubic-bezier(0.18, 0.89, 0.43, 1.19);
+
+      opacity: 1;
+
+      ${IconContainer} {
+        transition: opacity 0.3s ease-in-out 0.2s;
+
+        opacity: 1;
+      }
+
+      ${Profile} {
+        transition: opacity 0.3s ease-in-out 0.25s;
+
+        opacity: 1;
+      }
+
+      ${InfoContainer} {
+        transition: opacity 0.3s ease-in-out 0.3s;
+
+        opacity: 1;
+      }
+
+      ${AccordionContainer} {
+        transition: opacity 0.3s ease-in-out 0.35s;
+
+        opacity: 1;
+      }
+    `}
 `;
