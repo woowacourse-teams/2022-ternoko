942b89e91fec4d390ba7bb73644dad9950c3f6cb
kimsanglok <evertree6031@gmail.com>
2022-07-30 13:26:20 +0900

    feat: slack server redirect 방식 수정

--- frontend/src/pages/LoginPage/index.tsx
+++ frontend/src/pages/LoginPage/index.tsx
@@ -1,15 +1,12 @@
 import * as S from './styled';
-
 const logoURL =
   'https://user-images.githubusercontent.com/19251499/180158202-4d757b47-d804-45e4-b73b-9d6e13fe844b.png';
 const slackURL = 'https://a.slack-edge.com/80588/marketing/img/icons/icon_slack_hash_colored.png';
 
-const LoginPage = () => {
-  const handleClickLoginButton = () => {
-    location.href =
-      'https://slack.com/openid/connect/authorize?scope=openid%20email%20profile&amp;response_type=code&amp;redirect_uri=https://15.164.218.98:8080/api/login&amp;client_id=3756998338916.3821665111344 HTTP/1.1';
-  };
+const slackLoginURL =
+  'https://ternoko.slack.com/oauth?client_id=3756998338916.3821665111344&scope=&user_scope=openid%2Cemail%2Cprofile&redirect_uri=https%3A%2F%2Flocalhost:3000%2Fapi%2Flogin&state=&granular_bot_scope=1&single_channel=0&install_redirect=&tracked=1&openid_connect=1&response_type=code&team=';
 
+const LoginPage = () => {
   return (
     <S.Box>
       <S.LeftBox>
@@ -20,10 +17,12 @@ const LoginPage = () => {
             <h1>터놓고</h1>
             <img src={logoURL} alt="프로필 로고" />
           </S.LogoBox>
-          <S.LoginButton onClick={handleClickLoginButton}>
-            <img src={slackURL} alt="슬랙 로고" />
-            슬랙 로그인
-          </S.LoginButton>
+          <a target="_blank" href={slackLoginURL}>
+            <S.LoginButton>
+              <img src={slackURL} alt="슬랙 로고" />
+              슬랙 로그인
+            </S.LoginButton>
+          </a>
         </div>
       </S.LeftBox>
       <S.RightBox></S.RightBox>
