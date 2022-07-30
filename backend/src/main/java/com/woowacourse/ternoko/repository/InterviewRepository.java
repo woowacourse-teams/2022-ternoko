b38d052460b715612d8daeb5a87f68707717ae36
dongho <dongho108@naver.com>
2022-07-30 17:17:08 +0900

    feat: 인터뷰에서 날짜를 기준으로 조회해오는 기능 추가
    Co-authored-by: Juhyung990122 <harukawa99@naver.com>

--- backend/src/main/java/com/woowacourse/ternoko/repository/InterviewRepository.java
+++ backend/src/main/java/com/woowacourse/ternoko/repository/InterviewRepository.java
@@ -9,6 +9,16 @@ import org.springframework.data.jpa.repository.Query;
 public interface InterviewRepository extends JpaRepository<Interview, Long> {
 
     @Query("SELECT i FROM Interview i WHERE i.coach.id = :coachId and i.interviewStartTime BETWEEN :start and :end")
-    List<Interview> findAllByCoachIdAndDateRange(final LocalDateTime start, final LocalDateTime end,
+    List<Interview> findAllByCoachIdAndDateRange(final LocalDateTime start,
+                                                 final LocalDateTime end,
                                                  final Long coachId);
+
+    @Query("SELECT i "
+            + "FROM Interview i "
+            + "WHERE YEAR(i.interviewStartTime) = :year "
+            + "and MONTH(i.interviewStartTime) = :month "
+            + "and DAY(i.interviewStartTime) = :day")
+    List<Interview> findAllByInterviewStartDay(final int year,
+                                               final int month,
+                                               final int day);
 }
---
+++ backend/src/test/java/com/woowacourse/ternoko/repository/InterviewRepositoryTest.java
@@ -0,0 +1,59 @@
+package com.woowacourse.ternoko.repository;
+
+import static com.woowacourse.ternoko.fixture.MemberFixture.COACH1;
+import static com.woowacourse.ternoko.fixture.MemberFixture.COACH2;
+import static com.woowacourse.ternoko.fixture.ReservationFixture.FORM_ITEMS1;
+import static com.woowacourse.ternoko.fixture.ReservationFixture.FORM_ITEMS2;
+import static com.woowacourse.ternoko.fixture.ReservationFixture.FORM_ITEMS3;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.woowacourse.ternoko.domain.FormItem;
+import com.woowacourse.ternoko.domain.Interview;
+import com.woowacourse.ternoko.domain.member.Member;
+import java.time.LocalDateTime;
+import java.util.List;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InterviewRepositoryTest {
+
+    @Autowired
+    private InterviewRepository interviewRepository;
+
+    @Test
+    @DisplayName("해당 일시에 시작하는 interivew를 들고와야 한다.")
+    void findAllByInterviewStartDay() {
+        // given
+        Long savedInterviewId1 = saveInterview(LocalDateTime.of(2022, 7, 29, 17, 30),
+                COACH1, "애쉬", FORM_ITEMS1);
+        Long savedInterviewId2 = saveInterview(LocalDateTime.of(2022, 7, 29, 17, 30),
+                COACH2, "열음", FORM_ITEMS2);
+        saveInterview(LocalDateTime.of(2022, 7, 30, 17, 30),
+                COACH2, "바니", FORM_ITEMS3);
+
+        // when
+        final List<Interview> interviews = interviewRepository.findAllByInterviewStartDay(2022, 7, 29);
+
+        // then
+        assertThat(interviews.stream())
+                .map(Interview::getId)
+                .contains(savedInterviewId1, savedInterviewId2);
+    }
+
+    private Long saveInterview(final LocalDateTime localDateTime,
+                               final Member coach,
+                               final String crewNickname,
+                               final List<FormItem> formItems) {
+        final Interview interview = interviewRepository.save(new Interview(
+                localDateTime,
+                localDateTime.plusMinutes(30),
+                coach,
+                crewNickname,
+                formItems
+        ));
+        return interview.getId();
+    }
+}
