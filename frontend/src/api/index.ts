ef3e103294d9288833d9091531ab09847e544ac7
kimsanglok <evertree6031@gmail.com>
2022-08-02 10:48:11 +0900

    fix: 닉네임 중복검사 API url 수정
    Co-authored-by: soobin <sanaandmomo@users.noreply.github.com>

--- frontend/src/api/index.ts
+++ frontend/src/api/index.ts
@@ -45,4 +45,4 @@ export const patchCoachInfoAPI = (body: UserRequestBodyType) =>
   axios.patch(`${SERVER_URL}/api/coach/me`, body);
 
 export const getDuplicatedNicknameStatusAPI = (nickname: string) =>
-  axios.get(`${SERVER_URL}/api/login?nickname=${nickname}`);
+  axios.get(`${SERVER_URL}/api/login/check?nickname=${nickname}`);
