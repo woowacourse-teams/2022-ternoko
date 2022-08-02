dfaf915eb3b210af69c742f14e7793b6411ec0e8
kimsanglok <evertree6031@gmail.com>
2022-08-02 12:25:32 +0900

    feat: LocalStorage 관리 파일 개발
    Co-authored-by: soobin <sanaandmomo@users.noreply.github.com>

---
+++ frontend/src/localStorage.ts
@@ -0,0 +1,10 @@
+import { MemberRole } from '@/types/domain';
+
+const LocalStorage = {
+  getAccessToken: () => localStorage.getItem('accessToken'),
+  getMemberRole: () => localStorage.getItem('memberRole') as MemberRole,
+  setAccessToken: (accessToken: string) => localStorage.setItem('accessToken', accessToken),
+  setMemberRole: (memberRole: MemberRole) => localStorage.setItem('memberRole', memberRole),
+};
+
+export default LocalStorage;
