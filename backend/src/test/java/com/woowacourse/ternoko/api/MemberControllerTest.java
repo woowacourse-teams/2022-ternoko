de01963075c3342bdd73108c9faab40fdedbc126
Soomin Sohn <soomin.sohn.dev@gmail.com>
2022-08-02 13:16:55 +0900

    test: 회원 정보 api 문서 최신화

--- backend/src/test/java/com/woowacourse/ternoko/api/MemberControllerTest.java
+++ backend/src/test/java/com/woowacourse/ternoko/api/MemberControllerTest.java
@@ -2,24 +2,18 @@ package com.woowacourse.ternoko.api;
 
 import static com.woowacourse.ternoko.config.AuthorizationExtractor.AUTHORIZATION;
 import static com.woowacourse.ternoko.config.AuthorizationExtractor.BEARER_TYPE;
-import static com.woowacourse.ternoko.fixture.CoachAvailableTimeFixture.MONTH_REQUEST;
 import static com.woowacourse.ternoko.fixture.MemberFixture.COACH1;
 import static com.woowacourse.ternoko.fixture.MemberFixture.COACH3;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
-class MemberControllerTest extends RestDocsTestSupport {
-
-    private final ObjectMapper objectMapper = new ObjectMapper().registerModule(new JavaTimeModule());
+class MemberControllerTest extends ControllerTest {
 
     @Test
-    @DisplayName("코치 목록을 조회한다.")
+    @DisplayName("크루 - 코치 목록을 조회한다.")
     void findCoaches() throws Exception {
         mockMvc.perform(MockMvcRequestBuilders
                         .get("/api/coaches")
@@ -29,22 +23,12 @@ class MemberControllerTest extends RestDocsTestSupport {
     }
 
     @Test
-    @DisplayName("닉네임 중복 검사를 한다. ")
+    @DisplayName("[코치/크루] - 닉네임 중복 검사를 한다. ")
     void checkUniqueNickname() throws Exception {
         mockMvc.perform(MockMvcRequestBuilders
                         .get("/api/login/check")
                         .queryParam("nickname", COACH3.getNickname()))
                 .andExpect(status().isOk())
                 .andDo(restDocs.document());
-    }
-
-    private void createCalendarTimes(Long coachId) throws Exception {
-        mockMvc.perform(MockMvcRequestBuilders
-                        .put("/api/calendar/times")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .characterEncoding("utf-8")
-                        .header(AUTHORIZATION, BEARER_TYPE + jwtProvider.createToken(String.valueOf(coachId)))
-                        .content(objectMapper.writeValueAsString(MONTH_REQUEST)))
-                .andExpect(status().isOk());
     }
 }
