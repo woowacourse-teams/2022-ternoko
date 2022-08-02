1cd2b15fc9534f3b79dbee1a2afa2167711eb4cd
Soomin Sohn <soomin.sohn.dev@gmail.com>
2022-08-02 12:38:53 +0900

    test: 면담 예약 api 문서 최신화

--- backend/src/test/java/com/woowacourse/ternoko/api/ReservationControllerTest.java
+++ backend/src/test/java/com/woowacourse/ternoko/api/ReservationControllerTest.java
@@ -5,6 +5,7 @@ import static com.woowacourse.ternoko.config.AuthorizationExtractor.BEARER_TYPE;
 import static com.woowacourse.ternoko.fixture.MemberFixture.COACH1;
 import static com.woowacourse.ternoko.fixture.MemberFixture.CREW1;
 import static com.woowacourse.ternoko.fixture.ReservationFixture.COACH1_RESERVATION_REQUEST1;
+import static com.woowacourse.ternoko.fixture.ReservationFixture.COACH1_RESERVATION_REQUEST2;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import org.junit.jupiter.api.DisplayName;
@@ -15,9 +16,12 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 public class ReservationControllerTest extends ControllerTest {
 
     @Test
-    @DisplayName("면담 예약을 생성한다.")
-    void create() throws Exception {
+    @DisplayName("크루 - 면담 예약을 생성한다.")
+    void createReservation() throws Exception {
+        //given
         createCalendarTimes(COACH1.getId());
+
+        //when, then
         mockMvc.perform(MockMvcRequestBuilders
                         .post("/api/reservations")
                         .contentType(MediaType.APPLICATION_JSON)
@@ -29,7 +33,7 @@ public class ReservationControllerTest extends ControllerTest {
     }
 
     @Test
-    @DisplayName("면담 예약 상세 내역을 조회한다.")
+    @DisplayName("크루 - 면담 예약 내역을 조회한다.")
     void findReservationById() throws Exception {
         // given
         createCalendarTimes(COACH1.getId());
@@ -44,8 +48,8 @@ public class ReservationControllerTest extends ControllerTest {
     }
 
     @Test
-    @DisplayName("면담 예약 상세 내역 목록을 조회한다.")
-    void findAll() throws Exception {
+    @DisplayName("크루 - 면담 예약 내역 목록을 조회한다.")
+    void findAllReservations() throws Exception {
         // given
         createCalendarTimes(COACH1.getId());
         createReservations(CREW1.getId());
@@ -59,15 +63,53 @@ public class ReservationControllerTest extends ControllerTest {
     }
 
     @Test
-    @DisplayName("크루가 면담 예약을 취소한다.")
-    void delete() throws Exception {
+    @DisplayName("크루 - 면담 예약 내역을 수정한다.")
+    void updateReservation() throws Exception {
+        // given
         createCalendarTimes(COACH1.getId());
-        Long reservationId = createReservation(CREW1.getId());
+        final Long reservationId = createReservation(CREW1.getId());
+
+        // when, then
+        mockMvc.perform(MockMvcRequestBuilders
+                        .put("/api/reservations/{reservationId}", reservationId)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .characterEncoding("utf-8")
+                        .header(AUTHORIZATION, BEARER_TYPE + jwtProvider.createToken(String.valueOf(CREW1.getId())))
+                        .content(objectMapper.writeValueAsString(COACH1_RESERVATION_REQUEST2)))
+                .andExpect(status().isOk())
+                .andDo(restDocs.document());
+    }
+
+    @Test
+    @DisplayName("크루 - 면담 예약을 취소한다.")
+    void deleteReservation() throws Exception {
+        //given
+        createCalendarTimes(COACH1.getId());
+        final Long reservationId = createReservation(CREW1.getId());
+
+        //when, then
         mockMvc.perform(MockMvcRequestBuilders
                         .delete("/api/reservations/{reservationId}", reservationId)
                         .contentType(MediaType.APPLICATION_JSON)
                         .characterEncoding("utf-8")
                         .header(AUTHORIZATION, BEARER_TYPE + jwtProvider.createToken(String.valueOf(CREW1.getId()))))
+                .andExpect(status().isNoContent())
+                .andDo(restDocs.document());
+    }
+
+    @Test
+    @DisplayName("코치 - 면담 예약을 취소한다.")
+    void cancelReservation() throws Exception {
+        //given
+        createCalendarTimes(COACH1.getId());
+        final Long reservationId = createReservation(CREW1.getId());
+
+        //when, then
+        mockMvc.perform(MockMvcRequestBuilders
+                        .patch("/api/reservations/{reservationId}", reservationId)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .characterEncoding("utf-8")
+                        .header(AUTHORIZATION, BEARER_TYPE + jwtProvider.createToken(String.valueOf(COACH1.getId()))))
                 .andExpect(status().isNoContent())
                 .andDo(restDocs.document());
     }
