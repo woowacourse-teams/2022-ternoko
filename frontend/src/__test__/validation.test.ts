ccd104d354c8759559c6a5938dfa3f30f580a93c
kimsanglok <evertree6031@gmail.com>
2022-08-03 16:31:57 +0900

    test: 테스트명 수정

--- frontend/src/__test__/validation.test.ts
+++ frontend/src/__test__/validation.test.ts
@@ -11,7 +11,7 @@ import {
 } from '@/validations';
 
 describe('유효성 검사', () => {
-  test(`한 줄 소개의 길이는 ${INTRODUCE_MIN_LENGTH}를 넘어야 한다. `, () => {
+  test(`한 줄 소개의 길이는 ${INTRODUCE_MIN_LENGTH}이상이면, true를 반환한다. `, () => {
     const introduce = '안녕하세요. 저는 따뜻한 마음의 소유자입니다.';
 
     expect(isOverIntroduceMinLength(introduce)).toBe(true);
@@ -29,7 +29,7 @@ describe('유효성 검사', () => {
     expect(isValidNicknameLength(nickname)).toBe(false);
   });
 
-  test(`면담 신청 입력 폼의 길이는 ${CREW_APPLY_FORM_MIN_LENGTH} 이상이어야 `, () => {
+  test(`면담 신청 입력 폼의 길이는 ${CREW_APPLY_FORM_MIN_LENGTH}이상이면, true를 반환한다.`, () => {
     const input = '좋은 팀 문화가 무엇인지 궁금합니다.';
 
     expect(isOverApplyFormMinLength(input)).toBe(true);
