971db1ae923e81db706ea34dd752bc03096ffa84
Kimsanglok <evertree6031@gmail.com>
2022-08-04 14:37:12 +0900

    Update constants.ts

--- frontend/src/constants.ts
+++ frontend/src/constants.ts
@@ -2,7 +2,7 @@ export const PAGE = {
   LOGIN: '/login',
   LOGIN_REGISTER: '/login/register',
   SLACK_LOGIN_SERVER:
-    'https://ternoko.slack.com/oauth?client_id=3756998338916.3821665111344&scope=&user_scope=openid%2Cemail%2Cprofile&redirect_uri=https%3A%2F%2Fdev.ternoko.site%2Fapi%2Flogin&state=&granular_bot_scope=1&single_channel=0&install_redirect=&tracked=1&openid_connect=1&response_type=code&team=',
+    'https://ternoko.slack.com/oauth?client_id=3756998338916.3821665111344&scope=&user_scope=openid%2Cemail%2Cprofile&redirect_uri=https%3A%2F%2Fternoko.site%2Fapi%2Flogin&state=&granular_bot_scope=1&single_channel=0&install_redirect=&tracked=1&openid_connect=1&response_type=code&team=',
   CREW_HOME: '/',
   RESERVATION_APPLY: '/reservation/apply',
   RESERVATION_COMPLETE: '/reservation/complete',
