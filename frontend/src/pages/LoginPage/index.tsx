6d1779b68076851a71b0847e42ee8a8a9f1b81e4
kimsanglok <evertree6031@gmail.com>
2022-08-04 21:11:52 +0900

    feat: LoginPage - 터놓고 멤버 애니메이션 개발

--- frontend/src/pages/LoginPage/index.tsx
+++ frontend/src/pages/LoginPage/index.tsx
@@ -1,8 +1,63 @@
+import { useEffect, useState } from 'react';
+
 import * as S from './styled';
 
 import { PAGE } from '@/constants';
 
+export type TernokoMemberNickname = '앤지' | '애쉬' | '열음' | '바니' | '수달' | '아놀드' | '록바';
+export type TernokoMemberStatus = 'BORN' | 'ALIVE';
+
+type TernokoMember = {
+  nickname: TernokoMemberNickname;
+  imageUrl: string;
+  status: TernokoMemberStatus;
+};
+
+const ternokoMembers = [
+  {
+    nickname: '앤지',
+    imageUrl: '/assets/image/angel.png',
+    status: 'BORN',
+  },
+  {
+    nickname: '애쉬',
+    imageUrl: '/assets/image/ash.png',
+    status: 'BORN',
+  },
+  {
+    nickname: '열음',
+    imageUrl: '/assets/image/yeoleum.png',
+    status: 'BORN',
+  },
+  {
+    nickname: '바니',
+    imageUrl: '/assets/image/bunny.png',
+    status: 'BORN',
+  },
+  {
+    nickname: '수달',
+    imageUrl: '/assets/image/sudal.png',
+    status: 'BORN',
+  },
+  {
+    nickname: '아놀드',
+    imageUrl: '/assets/image/arnold.png',
+    status: 'BORN',
+  },
+  {
+    nickname: '록바',
+    imageUrl: '/assets/image/lokba.png',
+    status: 'BORN',
+  },
+] as TernokoMember[];
+
 const LoginPage = () => {
+  const [members, setMembers] = useState<TernokoMember[]>(ternokoMembers);
+
+  useEffect(() => {
+    setMembers((prev) => prev.map((member) => ({ ...member, status: 'ALIVE' })));
+  }, []);
+
   return (
     <S.Box>
       <S.LeftBox>
@@ -21,7 +76,13 @@ const LoginPage = () => {
           </a>
         </div>
       </S.LeftBox>
-      <S.RightBox></S.RightBox>
+      <S.RightBox>
+        {members.map(({ nickname, imageUrl, status }) => (
+          <S.TernokoProfile key={nickname} status={status} nickname={nickname}>
+            <img src={imageUrl} alt="터놓고 프로필" />
+          </S.TernokoProfile>
+        ))}
+      </S.RightBox>
     </S.Box>
   );
 };
