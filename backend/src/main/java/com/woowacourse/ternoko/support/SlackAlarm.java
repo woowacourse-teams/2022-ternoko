56cdbc465570ba2867495e324f0dde8208734948
Api_Dev <54317630+dongho108@users.noreply.github.com>
2022-08-04 21:15:04 +0900

    fix: 크루와 코치 메세지 반대로 가던 이슈 해결 (#165)

--- backend/src/main/java/com/woowacourse/ternoko/support/SlackAlarm.java
+++ backend/src/main/java/com/woowacourse/ternoko/support/SlackAlarm.java
@@ -1,8 +1,11 @@
 package com.woowacourse.ternoko.support;
 
+import com.slack.api.methods.SlackApiException;
 import com.slack.api.methods.impl.MethodsClientImpl;
 import com.slack.api.methods.request.chat.ChatPostMessageRequest;
 import com.woowacourse.ternoko.domain.Interview;
+import com.woowacourse.ternoko.domain.member.Member;
+import java.io.IOException;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import org.springframework.beans.factory.annotation.Value;
@@ -11,7 +14,7 @@ import org.springframework.stereotype.Component;
 @Component
 public class SlackAlarm {
 
-    private static final DateTimeFormatter dateFormat = DateTimeFormatter.ofPattern("yyyy년 MM월 dd일 (E) HH시 mm분");
+    private static final DateTimeFormatter DATE_FORMAT = DateTimeFormatter.ofPattern("yyyy년 MM월 dd일 (E) HH시 mm분");
 
     private final String botToken;
     private final MethodsClientImpl slackMethodClient;
@@ -22,22 +25,23 @@ public class SlackAlarm {
     }
 
     public void sendMessage(final Interview interview, final String message) throws Exception {
-        LocalDateTime interviewStartTime = interview.getInterviewStartTime();
-        String coachNickname = interview.getCoach().getNickname();
-        String crewNickname = interview.getCrew().getNickname();
+        postMessage(message, interview.getCrew(), interview.getCoach(), interview.getInterviewStartTime());
+        postMessage(message, interview.getCoach(), interview.getCrew(), interview.getInterviewStartTime());
+    }
 
-        ChatPostMessageRequest crewRequest = ChatPostMessageRequest.builder()
-                .text(String.format(message, coachNickname, crewNickname, dateFormat.format(interviewStartTime)))
-                .channel(interview.getCrew().getUserId())
+    private void postMessage(final String message,
+                             final Member receiver,
+                             final Member member,
+                             final LocalDateTime interviewStartTime)
+            throws IOException, SlackApiException {
+        final ChatPostMessageRequest crewRequest = ChatPostMessageRequest.builder()
+                .text(String.format(message,
+                        receiver.getNickname(),
+                        member.getNickname(),
+                        DATE_FORMAT.format(interviewStartTime)))
+                .channel(receiver.getUserId())
                 .token(botToken)
                 .build();
         slackMethodClient.chatPostMessage(crewRequest);
-
-        ChatPostMessageRequest coachRequest = ChatPostMessageRequest.builder()
-                .text(String.format(message, crewNickname, coachNickname, dateFormat.format(interviewStartTime)))
-                .channel(interview.getCoach().getUserId())
-                .token(botToken)
-                .build();
-        slackMethodClient.chatPostMessage(coachRequest);
     }
 }
