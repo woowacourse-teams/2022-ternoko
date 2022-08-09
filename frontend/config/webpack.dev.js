e48e5d806a38ca7095dd10c132d4f1b1ae8c886e
Kimsanglok <evertree6031@gmail.com>
2022-08-09 17:30:51 +0900

    [FE] feat: 빌드환경에 따른 환경변수를 설정한다.   (#186)
    * 머지 테스트
    
    * feat: 코치 면담 생성 페이지 개발
    
    * fix: 변경된 api 스펙에 맞게 수정
    
    Co-authored-by: Kimsanglok <evertree6031@gmail.com>
    
    * feat: Calendar 코치 스케줄 동기화
    
    Co-authored-by: Kimsanglok <evertree6031@gmail.com>
    
    * feat: 빌드환경에 따른 환경변수를 설정
    
    Co-authored-by: soobin <sanaandmomo@users.noreply.github.com>
    
    * refactor: 불필요한 webpack option 삭제
    
    * feat: production - 웹팩 설정 수정
    
    Co-authored-by: sanaandmomo <qajjang46@naver.com>
    Co-authored-by: soobin <sanaandmomo@users.noreply.github.com>

--- frontend/config/webpack.dev.js
+++ frontend/config/webpack.dev.js
@@ -1,28 +1,29 @@
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common');
 
-module.exports = merge(common, {
-  mode: 'development',
-  devtool: 'inline-source-map',
-  devServer: {
-    https: true,
-    open: false,
-    hot: true,
-    compress: true,
-    port: 3000,
-    historyApiFallback: true,
-    liveReload: true,
-  },
-  output: {
-    filename: '[name].[contenthash].js',
-    publicPath: '/',
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(sa|sc|c)ss$/i,
-        use: ['style-loader', 'css-loader'],
-      },
-    ],
-  },
-});
+module.exports = () =>
+  merge(common('development'), {
+    mode: 'development',
+    devtool: 'inline-source-map',
+    devServer: {
+      https: true,
+      open: false,
+      hot: true,
+      compress: true,
+      port: 3000,
+      historyApiFallback: true,
+      liveReload: true,
+    },
+    output: {
+      filename: '[name].[contenthash].js',
+      publicPath: '/',
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(sa|sc|c)ss$/i,
+          use: ['style-loader', 'css-loader'],
+        },
+      ],
+    },
+  });
--- frontend/src/api/index.ts
+++ frontend/src/api/index.ts
@@ -8,52 +8,54 @@ import {
 
 import LocalStorage from '@/localStorage';
 
-const SERVER_URL = 'https://dev.ternoko.site';
-
 const accessToken = LocalStorage.getAccessToken();
 
 if (accessToken) {
   axios.defaults.headers.common['Authorization'] = `Bearer ${accessToken}`;
 }
 
-export const getCoachesAPI = () => axios.get(`${SERVER_URL}/api/coaches`);
+export const getCoachesAPI = () => axios.get(`${process.env.SERVER_URL}/api/coaches`);
 
-export const getReservationsAPI = () => axios.get(`${SERVER_URL}/api/reservations`);
+export const getReservationsAPI = () => axios.get(`${process.env.SERVER_URL}/api/reservations`);
 
-export const getReservationAPI = (id: number) => axios.get(`${SERVER_URL}/api/reservations/${id}`);
+export const getReservationAPI = (id: number) =>
+  axios.get(`${process.env.SERVER_URL}/api/reservations/${id}`);
 
 export const postReservationAPI = (body: ReservationRequestBodyType) =>
-  axios.post(`${SERVER_URL}/api/reservations`, body);
+  axios.post(`${process.env.SERVER_URL}/api/reservations`, body);
 
 export const putReservationAPI = (reservationId: number, body: ReservationRequestBodyType) =>
-  axios.put(`${SERVER_URL}/api/reservations/${reservationId}`, body);
+  axios.put(`${process.env.SERVER_URL}/api/reservations/${reservationId}`, body);
 
 export const deleteCrewReservationAPI = (reservationId: number) =>
-  axios.delete(`${SERVER_URL}/api/reservations/${reservationId}`);
+  axios.delete(`${process.env.SERVER_URL}/api/reservations/${reservationId}`);
 
 export const deleteCoachReservationAPI = (reservationId: number) =>
-  axios.patch(`${SERVER_URL}/api/reservations/${reservationId}`);
+  axios.patch(`${process.env.SERVER_URL}/api/reservations/${reservationId}`);
 
 export const postCoachScheduleAPI = (body: CoachScheduleRequestBodyType) =>
-  axios.put(`${SERVER_URL}/api/calendar/times`, body);
+  axios.put(`${process.env.SERVER_URL}/api/calendar/times`, body);
 
 export const getCoachScheduleAPI = (coachId: number, year: number, month: number) =>
-  axios.get(`${SERVER_URL}/api/calendar/times?coachId=${coachId}&year=${year}&month=${month}`);
+  axios.get(
+    `${process.env.SERVER_URL}/api/calendar/times?coachId=${coachId}&year=${year}&month=${month}`,
+  );
 
 export const getCoachReservationAPI = (year: number, month: number) =>
-  axios.get(`${SERVER_URL}/api/schedules?year=${year}&month=${month}`);
+  axios.get(`${process.env.SERVER_URL}/api/schedules?year=${year}&month=${month}`);
 
-export const getUserStatusAPI = (code: string) => axios.get(`${SERVER_URL}/api/login?code=${code}`);
+export const getUserStatusAPI = (code: string) =>
+  axios.get(`${process.env.SERVER_URL}/api/login?code=${code}`);
 
-export const getCrewInfoAPI = () => axios.get(`${SERVER_URL}/api/crews/me`);
+export const getCrewInfoAPI = () => axios.get(`${process.env.SERVER_URL}/api/crews/me`);
 
-export const getCoachInfoAPI = () => axios.get(`${SERVER_URL}/api/coaches/me`);
+export const getCoachInfoAPI = () => axios.get(`${process.env.SERVER_URL}/api/coaches/me`);
 
 export const patchCrewInfoAPI = (body: UserRequestBodyType) =>
-  axios.patch(`${SERVER_URL}/api/crews/me`, body);
+  axios.patch(`${process.env.SERVER_URL}/api/crews/me`, body);
 
 export const patchCoachInfoAPI = (body: UserRequestBodyType) =>
-  axios.patch(`${SERVER_URL}/api/coaches/me`, body);
+  axios.patch(`${process.env.SERVER_URL}/api/coaches/me`, body);
 
 export const getDuplicatedNicknameStatusAPI = (nickname: string) =>
-  axios.get(`${SERVER_URL}/api/login/check?nickname=${nickname}`);
+  axios.get(`${process.env.SERVER_URL}/api/login/check?nickname=${nickname}`);
