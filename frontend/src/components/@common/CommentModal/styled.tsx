8f016f81dda8a9d1a13aeeb6cdc349dc1ffba592
kimsanglok <evertree6031@gmail.com>
2022-08-11 21:28:50 +0900

    feat: CommentModal 컴포넌트 개발
    Co-authored-by: soobin <sanaandmomo@users.noreply.github.com>

---
+++ frontend/src/components/@common/CommentModal/styled.tsx
@@ -0,0 +1,40 @@
+import styled from 'styled-components';
+
+export const TextArea = styled.textarea`
+  width: 100%;
+  height: 70px;
+  border-radius: 10px;
+  padding: 1rem;
+
+  font-size: 1.4rem;
+  resize: none;
+  outline: none;
+
+  border: 1px solid ${({ theme }) => theme.colors.gray_150};
+`;
+
+export const Icon = styled.img`
+  position: absolute;
+  top: 1rem;
+  right: 1rem;
+  width: 2rem;
+  height: 2rem;
+  object-fit: cover;
+  cursor: pointer;
+`;
+
+export const ExampleBox = styled.div`
+  margin: 1rem 0 3rem;
+  color: ${({ theme }) => theme.colors.gray_200};
+
+  > p {
+    line-height: 1.5;
+  }
+`;
+
+export const ButtonBox = styled.div`
+  display: flex;
+  justify-content: flex-end;
+  justify-content: center;
+  margin-top: 1.5rem;
+`;
