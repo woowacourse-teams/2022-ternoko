7eba1f88e085b96b172add7718a01c8f1f44e605
hyeonbinsa <shb1833@gmail.com>
2022-08-13 00:51:47 +0900

    feat: Comment Create 기능 구현 및 테스트 추가

--- backend/src/main/java/com/woowacourse/ternoko/comment/domain/Comment.java
+++ backend/src/main/java/com/woowacourse/ternoko/comment/domain/Comment.java
@@ -1,6 +1,7 @@
 package com.woowacourse.ternoko.comment.domain;
 
 import com.woowacourse.ternoko.comment.dto.CommentRequest;
+import com.woowacourse.ternoko.domain.member.Member;
 import com.woowacourse.ternoko.interview.domain.Interview;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -8,11 +9,20 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @Entity
+@Table(
+        uniqueConstraints = {
+                @UniqueConstraint(
+                        columnNames = {"member_id", "interview_id"}
+                )
+        }
+)
 @NoArgsConstructor
 public class Comment {
 
