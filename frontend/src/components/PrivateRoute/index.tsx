4908f8c9ab7beccf45de39173d720bb939dc56e3
kimsanglok <evertree6031@gmail.com>
2022-08-12 18:09:20 +0900

    feat: PrivateRoute - Suspense 기능 추가

--- frontend/src/components/PrivateRoute/index.tsx
+++ frontend/src/components/PrivateRoute/index.tsx
@@ -1,9 +1,55 @@
+import { Suspense } from 'react';
 import { Navigate, Outlet } from 'react-router-dom';
 
-import { PAGE } from '@/constants';
+import { MemberExtendedRole } from '@/types/domain';
 
-const PrivateRoute = () => {
-  return localStorage.getItem('accessToken') ? <Outlet /> : <Navigate to={PAGE.LOGIN} />;
+import { validateAccessTokenAPI } from '@/api';
+import { PAGE } from '@/constants';
+import LocalStorage from '@/localStorage';
+
+type PrivateRouteProps = {
+  auth: MemberExtendedRole;
+};
+
+type PendingProps = {
+  resource: { read: () => Promise<void> | boolean };
+};
+
+const Pending = ({ resource }: PendingProps) => {
+  const result = resource.read();
+
+  return result ? <Outlet /> : <Navigate to={PAGE.ACCESS_DENY} />;
+};
+
+const fetchAccessResult = (role: MemberExtendedRole) => {
+  let result: boolean | null = null;
+
+  const suspender = validateAccessTokenAPI(role).then((response) => {
+    result = response.data;
+  });
+
+  return {
+    read() {
+      if (result === null) {
+        return suspender;
+      }
+      return result;
+    },
+  };
+};
+
+const PrivateRoute = ({ auth }: PrivateRouteProps) => {
+  const accessToken = LocalStorage.getAccessToken();
+
+  if (!accessToken) {
+    return <Navigate to={PAGE.LOGIN} />;
+  }
+
+  return (
+    <Suspense fallback={<p>loading...</p>}>
+      <Pending resource={fetchAccessResult(auth)} />
+    </Suspense>
+  );
 };
 
 export default PrivateRoute;
