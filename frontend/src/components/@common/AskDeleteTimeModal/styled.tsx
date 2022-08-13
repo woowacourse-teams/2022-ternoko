66a352ad682fa252bf628aea71708783750fdbf9
sanaandmomo <qajjang46@naver.com>
2022-08-13 16:58:19 +0900

    feat: 코치에게 시간 삭제 묻는 모달 초안

---
+++ frontend/src/components/@common/AskDeleteTimeModal/styled.tsx
@@ -0,0 +1,18 @@
+import styled from 'styled-components';
+
+export const Icon = styled.img`
+  position: absolute;
+  top: 1rem;
+  right: 1rem;
+  width: 2rem;
+  height: 2rem;
+  object-fit: cover;
+  cursor: pointer;
+`;
+
+export const ButtonBox = styled.div`
+  display: flex;
+  justify-content: flex-end;
+  justify-content: center;
+  margin-top: 1.5rem;
+`;
