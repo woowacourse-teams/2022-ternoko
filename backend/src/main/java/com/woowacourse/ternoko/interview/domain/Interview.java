4b381dd1fc94f24b3f623e4f78d649790e8c4987
hyeonbinsa <shb1833@gmail.com>
2022-08-13 10:45:31 +0900

    refactor: 코드 스타일 체크

--- backend/src/main/java/com/woowacourse/ternoko/interview/domain/Interview.java
+++ backend/src/main/java/com/woowacourse/ternoko/interview/domain/Interview.java
@@ -1,11 +1,11 @@
 package com.woowacourse.ternoko.interview.domain;
 
-import com.woowacourse.ternoko.domain.InterviewStatusType;
 import static com.woowacourse.ternoko.common.exception.ExceptionType.CANNOT_EDIT_INTERVIEW;
 import static com.woowacourse.ternoko.domain.InterviewStatusType.FIXED;
 
 import com.woowacourse.ternoko.common.exception.ExceptionType;
 import com.woowacourse.ternoko.common.exception.InterviewStatusException;
+import com.woowacourse.ternoko.domain.InterviewStatusType;
 import com.woowacourse.ternoko.domain.MemberType;
 import com.woowacourse.ternoko.domain.member.Coach;
 import com.woowacourse.ternoko.domain.member.Crew;
