021d7d2bf7172248a128b320adf8de78fb435290
Juhyung990122 <harukawa99@naver.com>
2022-08-15 02:58:03 +0900

    style: 코드 포멧팅

--- backend/src/main/java/com/woowacourse/ternoko/common/exception/ExceptionResponse.java
+++ backend/src/main/java/com/woowacourse/ternoko/common/exception/ExceptionResponse.java
@@ -9,10 +9,11 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class ExceptionResponse {
+
     private int code;
     private String message;
 
-    public static ExceptionResponse from(final CommonException e){
+    public static ExceptionResponse from(final CommonException e) {
         return new ExceptionResponse(e.getCode().value(), e.getMessage());
     }
 }
