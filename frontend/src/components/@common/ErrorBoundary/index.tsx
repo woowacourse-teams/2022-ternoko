02173e3f59ee42ad5c278d86b5338fd06585cdc8
sanaandmomo <qajjang46@naver.com>
2022-08-16 14:05:06 +0900

    feat: ErrorBoundary 컴포넌트 생성
    Co-authored-by: Kimsanglok <evertree6031@gmail.com>

---
+++ frontend/src/components/@common/ErrorBoundary/index.tsx
@@ -0,0 +1,43 @@
+import { Component } from 'react';
+import { Navigate } from 'react-router-dom';
+
+import { PAGE } from '@/constants';
+
+type ErrorBoundaryProps = {
+  children?: React.ReactNode;
+};
+
+type ErrorBoundaryState = {
+  hasError: boolean;
+};
+
+class ErrorBoundary extends Component<ErrorBoundaryProps, ErrorBoundaryState> {
+  constructor(props: ErrorBoundaryProps) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  componentDidCatch() {
+    this.setState({
+      hasError: true,
+    });
+  }
+
+  render() {
+    const { children } = this.props;
+
+    console.log('errorboundary hasserror', this.state.hasError);
+
+    if (this.state.hasError) {
+      this.setState({
+        hasError: false,
+      });
+
+      return <Navigate to={PAGE.ACCESS_DENY} />;
+    }
+
+    return children;
+  }
+}
+
+export default ErrorBoundary;
