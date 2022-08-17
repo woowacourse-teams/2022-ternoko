869891a6e82b28a3b01fe4c67af3415db37a8dc9
bin_sa <36189291+HyeonbinSa@users.noreply.github.com>
2022-08-17 21:32:20 +0900

    [BE] fix: 슬랙 메세지 디자인 수정 (#283)
    * refactor: DatabaseInitializer 터놓고 데이터 수정(slack user id)
    
    * refactor: 슬랙 알람 메세지 수정
    
    * refactor: Home 링크 URL - 코치, 크루 분리
    
    * refactor: Code Style 수정
    
    * refactor: Code Style 수정

--- backend/src/main/java/com/woowacourse/ternoko/support/SlackAlarm.java
+++ backend/src/main/java/com/woowacourse/ternoko/support/SlackAlarm.java
@@ -3,18 +3,29 @@ package com.woowacourse.ternoko.support;
 import com.slack.api.methods.SlackApiException;
 import com.slack.api.methods.impl.MethodsClientImpl;
 import com.slack.api.methods.request.chat.ChatPostMessageRequest;
-import com.woowacourse.ternoko.domain.member.Member;
+import com.slack.api.model.Attachment;
+import com.slack.api.model.block.ContextBlock;
+import com.slack.api.model.block.DividerBlock;
+import com.slack.api.model.block.HeaderBlock;
+import com.slack.api.model.block.SectionBlock;
+import com.slack.api.model.block.composition.MarkdownTextObject;
+import com.slack.api.model.block.composition.PlainTextObject;
+import com.slack.api.model.block.element.ImageElement;
 import com.woowacourse.ternoko.interview.domain.Interview;
 import java.io.IOException;
-import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.List;
+import java.util.Locale;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
 public class SlackAlarm {
 
-    private static final DateTimeFormatter DATE_FORMAT = DateTimeFormatter.ofPattern("yyyy년 MM월 dd일 (E) HH시 mm분");
+    private static final DateTimeFormatter DATE_FORMAT = DateTimeFormatter.ofPattern("yyyy년 MM월 dd일(E) HH시 mm분",
+            Locale.KOREAN);
+    private static final String TERNOKO_CREW_URL = "https://ternoko.site/";
+    private static final String TERNOKO_COACH_URL = "https://ternoko.site/coach/home";
 
     private final String botToken;
     private final MethodsClientImpl slackMethodClient;
@@ -24,24 +35,75 @@ public class SlackAlarm {
         this.botToken = botToken;
     }
 
-    public void sendMessage(final Interview interview, final String message) throws Exception {
-        postMessage(message, interview.getCrew(), interview.getCoach(), interview.getInterviewStartTime());
-        postMessage(message, interview.getCoach(), interview.getCrew(), interview.getInterviewStartTime());
+    public void sendMessage(final Interview interview, final SlackMessageType slackMessageType) throws Exception {
+        postCrewMessage(slackMessageType, interview);
+        postCoachMessage(slackMessageType, interview);
     }
 
-    private void postMessage(final String message,
-                             final Member receiver,
-                             final Member member,
-                             final LocalDateTime interviewStartTime)
-            throws IOException, SlackApiException {
+    private void postCrewMessage(final SlackMessageType slackMessageType,
+                                 final Interview interview) throws IOException, SlackApiException {
         final ChatPostMessageRequest crewRequest = ChatPostMessageRequest.builder()
-                .text(String.format(message,
-                        receiver.getNickname(),
-                        member.getNickname(),
-                        DATE_FORMAT.format(interviewStartTime)))
-                .channel(receiver.getUserId())
+                .text(String.format(slackMessageType.getCrewPreviewMessage(), interview.getCoach().getNickname()))
+                .attachments(generateAttachment(slackMessageType, interview, TERNOKO_CREW_URL))
+                .channel(interview.getCrew().getUserId())
                 .token(botToken)
                 .build();
         slackMethodClient.chatPostMessage(crewRequest);
+    }
+
+    private void postCoachMessage(final SlackMessageType slackMessageType,
+                                  final Interview interview) throws IOException, SlackApiException {
+        final ChatPostMessageRequest coachRequest = ChatPostMessageRequest.builder()
+                .text(String.format(slackMessageType.getCoachPreviewMessage(), interview.getCrew().getNickname()))
+                .attachments(generateAttachment(slackMessageType, interview, TERNOKO_COACH_URL))
+                .channel(interview.getCoach().getUserId())
+                .token(botToken)
+                .build();
+        slackMethodClient.chatPostMessage(coachRequest);
+    }
+
+    private List<Attachment> generateAttachment(final SlackMessageType slackMessageType,
+                                                final Interview interview,
+                                                final String homeUrl) {
+        return List.of(Attachment.builder()
+                .color(slackMessageType.getColor())
+                .blocks(List.of(HeaderBlock.builder()
+                                        .text(PlainTextObject.builder().text(slackMessageType.getAttachmentMessage()).build())
+                                        .build(),
+                                DividerBlock.builder().build(),
+                                SectionBlock.builder().fields(
+                                        List.of(MarkdownTextObject.builder()
+                                                        .text(":clock3: *면담일시*"
+                                                                + System.lineSeparator()
+                                                                + DATE_FORMAT.format(interview.getInterviewStartTime()))
+                                                        .build(),
+                                                MarkdownTextObject.builder()
+                                                        .text(":smiley: *크루*"
+                                                                + System.lineSeparator()
+                                                                + interview.getCrew().getNickname())
+                                                        .build(),
+                                                MarkdownTextObject.builder()
+                                                        .text(" ")
+                                                        .build(),
+                                                MarkdownTextObject.builder()
+                                                        .text(":smiley: *코치*"
+                                                                + System.lineSeparator()
+                                                                + interview.getCoach().getNickname())
+                                                        .build())
+                                ).build(),
+                                ContextBlock.builder().elements(List.of(
+                                                MarkdownTextObject.builder()
+                                                        .text("<" + homeUrl + "|터놓고로 이동>").build(),
+                                                ImageElement.builder()
+                                                        .imageUrl(interview.getCoach().getImageUrl())
+                                                        .altText("코치 이미지")
+                                                        .build(),
+                                                ImageElement.builder()
+                                                        .imageUrl(interview.getCrew().getImageUrl())
+                                                        .altText("크루 이미지")
+                                                        .build()))
+                                        .build()
+                        )
+                ).build());
     }
 }
