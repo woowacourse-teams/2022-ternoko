ce60fb878cbc7b45e51c7f473d5032075526adab
sanaandmomo <qajjang46@naver.com>
2022-08-18 21:32:19 +0900

    feat: NotFoundPage 개발

--- frontend/src/AppRoutes.tsx
+++ frontend/src/AppRoutes.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Outlet, Route, Routes } from 'react-router-dom';
 
 import styled from 'styled-components';
 
+import AccessDenyPage from '@/pages/AccessDenyPage';
 import CoachHomePage from '@/pages/CoachHomePage';
 import CoachInterviewCreatePage from '@/pages/CoachInterviewCreatePage';
 import HomePage from '@/pages/HomePage';
@@ -39,8 +40,8 @@ const AppRoutes = () => {
       <Routes>
         <Route path={PAGE.LOGIN} element={<LoginPage />} />
         <Route path={PAGE.OAUTH_REDIRECT} element={<OAuthRedirectHandlerPage />} />
-        <Route path={PAGE.ACCESS_DENY} element={<NotFoundPage type="DENY" />} />
         <Route path={PAGE.NOT_FOUND} element={<NotFoundPage type="DEFAULT" />} />
+        <Route path={PAGE.ACCESS_DENY} element={<NotFoundPage type="DENY" />} />
 
         <Route path={PAGE.BASE} element={<Layout />}>
           <Route element={<PrivateRoute auth="ALL" />}>
