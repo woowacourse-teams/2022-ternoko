2665d440984a98199fa31c7bbcf9111254087296
sanaandmomo <qajjang46@naver.com>
2022-08-18 18:07:59 +0900

    fix: 리뷰 반영

--- frontend/src/components/@common/TernokoLoading/index.tsx
+++ frontend/src/components/@common/TernokoLoading/index.tsx
@@ -1,12 +1,12 @@
 import Loading from '@/components/@common/Loading';
 
+const additionalBoxStyle = `
+  position: fixed; left: 50%; top: 50%; transform: translate(-50%, -50%); width: 50%; height: 50%; background-color: unset;
+`;
+
 const TernokoLoading = () => {
   return (
-    <Loading
-      additionalBoxStyle="position: fixed; left: 50%; top: 50%; transform: translate(-50%, -50%); width: 50%; height: 50%; background-color: unset;"
-      profileSizeRem={25}
-      animationDuration={1.2}
-    />
+    <Loading additionalBoxStyle={additionalBoxStyle} profileSizeRem={25} animationDuration={1.2} />
   );
 };
 
