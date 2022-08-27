618fa8c0ad3da891ef69def3ea6ec85308bd1dc7
her0807 <her0807@naver.com>
2022-08-27 17:18:12 +0900

    test: CrewControllerTest SpringBootTest 제거

--- backend/src/test/java/com/woowacourse/ternoko/api/CrewControllerTest.java
+++ backend/src/test/java/com/woowacourse/ternoko/api/CrewControllerTest.java
@@ -4,18 +4,33 @@ import static com.woowacourse.ternoko.login.presentation.AuthorizationExtractor.
 import static com.woowacourse.ternoko.login.presentation.AuthorizationExtractor.BEARER_TYPE;
 import static com.woowacourse.ternoko.support.fixture.MemberFixture.CREW2;
 import static com.woowacourse.ternoko.support.fixture.MemberFixture.CREW2_UPDATE_REQUEST;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.doNothing;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import com.woowacourse.ternoko.controller.CrewController;
+import com.woowacourse.ternoko.dto.CrewResponse;
+import com.woowacourse.ternoko.service.CrewService;
+import com.woowacourse.ternoko.support.utils.WebMVCTest;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
-public class CrewControllerTest extends ControllerSupporter {
+@WebMvcTest(CrewController.class)
+public class CrewControllerTest extends WebMVCTest {
+
+    @MockBean
+    private CrewService crewService;
 
     @Test
     @DisplayName("크루 - 내 정보를 조회한다.")
     void findCrew() throws Exception {
+        given(crewService.findCrew(any()))
+                .willReturn(CrewResponse.from(CREW2));
         // given, when, then
         mockMvc.perform(MockMvcRequestBuilders
                         .get("/api/crews/me")
@@ -27,6 +42,7 @@ public class CrewControllerTest extends ControllerSupporter {
     @Test
     @DisplayName("크루 - 내 정보를 수정한다.")
     void updateCrew() throws Exception {
+        doNothing().when(crewService).partUpdateCrew(any(), any());
         // given, when, then
         mockMvc.perform(MockMvcRequestBuilders
                         .patch("/api/crews/me")
