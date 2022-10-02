ee0b86db7a6ae35e0d78fc8bd06b2ec9245e775a
kimsanglok <evertree6031@gmail.com>
2022-10-02 19:55:27 +0900

    feat: AllScheduleByMonthModal 컴포넌트 개발
    (cherry picked from commit d771b9c1d46a92a7e98041c73aed52f5a0fa9946)

---
+++ frontend/src/components/@common/AllScheduleByMonthModal/index.tsx
@@ -0,0 +1,84 @@
+import * as S from './styled';
+
+import Modal from '@/components/@common/Modal';
+
+import { CalendarTime, StringDictionary } from '@/types/domain';
+
+import { separateFullDate } from '@/utils';
+
+type AllScheduleByMonthModalProps = {
+  show: boolean;
+  display: boolean;
+  calendarTime: CalendarTime;
+  handleCloseModal: () => void;
+};
+
+type TrimCalendarTimeType = {
+  year: number;
+  month: number;
+  day: number;
+  times: string[];
+};
+
+const trimCalendarTime = (calendarTime: CalendarTime) => {
+  let index = -1;
+
+  return calendarTime.times.reduce((trimmedCalendarTime, fullDate) => {
+    const { year, month, day, time } = separateFullDate(fullDate);
+    const yearAsNumber = Number(year);
+    const monthAsNumber = Number(month);
+    const dayAsNumber = Number(day);
+
+    if (index !== -1 && trimmedCalendarTime[index].day === dayAsNumber) {
+      trimmedCalendarTime[index].times = [...trimmedCalendarTime[index].times, time];
+    } else {
+      trimmedCalendarTime[++index] = {
+        year: yearAsNumber,
+        month: monthAsNumber,
+        day: dayAsNumber,
+        times: [time],
+      };
+    }
+
+    return trimmedCalendarTime;
+  }, [] as TrimCalendarTimeType[]);
+};
+
+const AllScheduleByMonthModal = ({
+  show,
+  display,
+  calendarTime,
+  handleCloseModal,
+}: AllScheduleByMonthModalProps) => {
+  const trimmedCalendarTime = calendarTime ? trimCalendarTime(calendarTime) : [];
+
+  return (
+    <Modal
+      show={show}
+      display={display}
+      handleCloseModal={handleCloseModal}
+      additionalFrameStyle={S.additionalFrameStyle}
+    >
+      <S.Box>
+        {trimmedCalendarTime.length === 0 && (
+          <S.EmptyMessage>생성한 면담 스케쥴이 없습니다.🤣</S.EmptyMessage>
+        )}
+        {trimmedCalendarTime.length !== 0 &&
+          trimmedCalendarTime.map(({ year, month, day, times }) => (
+            <div>
+              <S.FullDate>
+                {year}년 {month}월 {day}일
+              </S.FullDate>
+              <S.TimeContainer>
+                {times.map((time) => (
+                  <S.Time>{time}</S.Time>
+                ))}
+              </S.TimeContainer>
+            </div>
+          ))}
+      </S.Box>
+    </Modal>
+  );
+};
+
+export default AllScheduleByMonthModal;
