c7b5280eaacd27f8106f9d9a33c816f6cb061e66
Soomin Sohn <soomin.sohn.dev@gmail.com>
2022-10-06 12:51:55 +0900

    refactor: partUpdateCrew 메서드 중복 닉네임 검증 로직 추가

--- backend/src/main/java/com/woowacourse/ternoko/core/application/CrewService.java
+++ backend/src/main/java/com/woowacourse/ternoko/core/application/CrewService.java
@@ -1,7 +1,11 @@
 package com.woowacourse.ternoko.core.application;
 
+import static com.woowacourse.ternoko.common.exception.ExceptionType.DUPLICATED_MEMBER_NICKNAME;
+
 import com.woowacourse.ternoko.common.exception.CrewNotFoundException;
 import com.woowacourse.ternoko.common.exception.ExceptionType;
+import com.woowacourse.ternoko.common.exception.InvalidMemberNicknameException;
+import com.woowacourse.ternoko.core.domain.member.MemberRepository;
 import com.woowacourse.ternoko.core.domain.member.crew.Crew;
 import com.woowacourse.ternoko.core.domain.member.crew.CrewRepository;
 import com.woowacourse.ternoko.core.dto.request.CrewUpdateRequest;
@@ -18,8 +22,16 @@ import org.springframework.transaction.annotation.Transactional;
 public class CrewService {
 
     private final CrewRepository crewRepository;
+    private final MemberRepository memberRepository;
 
     public void partUpdateCrew(final Long crewId, final CrewUpdateRequest request) {
+        final Crew crew = getCrewById(crewId);
+        final String requestNickname = request.getNickname();
+
+        if (!crew.isSameNickname(requestNickname)
+                && memberRepository.existsByNickname(request.getNickname())) {
+            throw new InvalidMemberNicknameException(DUPLICATED_MEMBER_NICKNAME);
+        }
         crewRepository.updateNickNameAndImageUrl(crewId, request.getNickname(), request.getImageUrl());
     }
 
--- backend/src/test/java/com/woowacourse/ternoko/service/CrewServiceTest.java
+++ backend/src/test/java/com/woowacourse/ternoko/service/CrewServiceTest.java
@@ -1,10 +1,13 @@
 package com.woowacourse.ternoko.service;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertAll;
 
+import com.woowacourse.ternoko.common.exception.InvalidMemberNicknameException;
 import com.woowacourse.ternoko.core.application.CrewService;
 import com.woowacourse.ternoko.core.domain.member.crew.Crew;
+import com.woowacourse.ternoko.core.domain.member.crew.CrewRepository;
 import com.woowacourse.ternoko.core.dto.request.CrewUpdateRequest;
 import com.woowacourse.ternoko.core.dto.response.CrewResponse;
 import com.woowacourse.ternoko.support.utils.DatabaseSupporter;
@@ -19,6 +22,9 @@ public class CrewServiceTest extends DatabaseSupporter {
     @Autowired
     private CrewService crewService;
 
+    @Autowired
+    private CrewRepository crewRepository;
+
     @Test
     @DisplayName("slack 회원가입 후 닉네임과 이미지를 입력받아 partUpdate 한다")
     void partUpdateCrew() {
@@ -30,11 +36,49 @@ public class CrewServiceTest extends DatabaseSupporter {
         // when
         crewService.partUpdateCrew(savedCrew.getId(), new CrewUpdateRequest(nickname, imageUrl));
         final CrewResponse updatedCrew = crewService.findCrew(savedCrew.getId());
-        // then
 
+        // then
         assertAll(
                 () -> assertThat(updatedCrew.getImageUrl()).isEqualTo(imageUrl),
                 () -> assertThat(updatedCrew.getNickname()).isEqualTo(nickname)
         );
+
+        crewRepository.deleteById(savedCrew.getId());
+    }
+
+    @Test
+    @DisplayName("slack 회원가입 후 닉네임 변경 시 본인의 닉네임으로 변경 가능하다.")
+    void partUpdateCrewSameNickName() {
+        // given
+        final Crew savedCrew = crewService.save(new Crew("엔젤앤지", "데빌앤지", "sudal@gmail.com", "U123456789", "기본이미지"));
+        final String nickname = "데빌앤지";
+        final String imageUrl = "탑건2.png";
+
+        // when
+        crewService.partUpdateCrew(savedCrew.getId(), new CrewUpdateRequest(nickname, imageUrl));
+        final CrewResponse updatedCrew = crewService.findCrew(savedCrew.getId());
+
+        // then
+        assertAll(
+                () -> assertThat(updatedCrew.getImageUrl()).isEqualTo(imageUrl),
+                () -> assertThat(updatedCrew.getNickname()).isEqualTo(nickname)
+        );
+
+        crewRepository.deleteById(savedCrew.getId());
+    }
+
+    @Test
+    @DisplayName("slack 회원가입 후 닉네임변경 시 본인의 닉네임이 아니면서 이미 존재하는 회원 닉네임이면 예외를 반환한다.")
+    void partUpdateCrewException() {
+        // given
+        final Crew savedCrew = crewService.save(new Crew("톰크루즈", null, "sudal@gmail.com", "U123456789", "기본이미지"));
+        final String existNickname = "앤지";
+        final String imageUrl = "탑건2.png";
+
+        // when, then
+        assertThatThrownBy(() ->  crewService.partUpdateCrew(savedCrew.getId(), new CrewUpdateRequest(existNickname, imageUrl)))
+                .isInstanceOf(InvalidMemberNicknameException.class);
+
+        crewRepository.deleteById(savedCrew.getId());
     }
 }
