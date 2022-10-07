434edc515ffc028ded1e35d7f197139f7feb9140
sanaandmomo <qajjang46@naver.com>
2022-10-07 13:52:46 +0900

    refactor: 캘린더 관련 필요 없는 파일 제거

--- frontend/src/components/@common/CommonCrewAndCoachCalendarStyle/styled.tsx
+++ frontend/src/components/@common/CommonCrewAndCoachCalendarStyle/styled.tsx
@@ -1,6 +1,4 @@
-import styled, { css } from 'styled-components';
-
-import { Day } from '@/components/@common/CalendarStyle/styled';
+import styled, { css, keyframes } from 'styled-components';
 
 import { DayType } from '@/types/domain';
 
@@ -19,12 +17,89 @@ type CalendarDayProps = {
   mark?: boolean;
 };
 
-export const CalendarDay = styled(Day)<CalendarDayProps>`
+const toTop = keyframes`
+  0% {
+    transform: translateY(100%);
+    opacity: 0;
+  }
+  100% {
+    transform: translateY(0);
+    opacity: 1;
+  }
+`;
+
+export const CalendarDay = styled.div<CalendarDayProps>`
   position: relative;
   display: grid;
   place-items: center;
+  padding: 0.5rem;
   width: 6rem;
   height: 6rem;
+  animation: ${toTop} 1s;
+  cursor: pointer;
+
+  span {
+    position: absolute;
+  }
+
+  span:nth-of-type(1),
+  span:nth-of-type(3) {
+    width: 2px;
+    height: 0;
+    background-color: ${({ theme }) => theme.colors.pink_50};
+  }
+
+  span:nth-of-type(1) {
+    bottom: 0;
+    left: 0;
+  }
+
+  span:nth-of-type(3) {
+    top: 0;
+    right: 0;
+  }
+
+  span:nth-of-type(2),
+  span:nth-of-type(4) {
+    width: 0;
+    height: 2px;
+    background-color: ${({ theme }) => theme.colors.pink_50};
+  }
+
+  span:nth-of-type(2) {
+    top: 0;
+    left: 0;
+  }
+
+  span:nth-of-type(4) {
+    bottom: 0;
+    right: 0;
+  }
+
+  :hover span {
+    transition: width 0.15s ease-in-out, height 0.15s ease-in-out;
+  }
+
+  :hover span:nth-of-type(1),
+  :hover span:nth-of-type(3) {
+    height: 100%;
+  }
+
+  :hover span:nth-of-type(2),
+  :hover span:nth-of-type(4) {
+    width: 100%;
+  }
+  :hover span:nth-of-type(2) {
+    transition-delay: 0.15s;
+  }
+
+  :hover span:nth-of-type(3) {
+    transition-delay: 0.3s;
+  }
+
+  :hover span:nth-of-type(4) {
+    transition-delay: 0.45s;
+  }
 
   @media ${({ theme }) => theme.devices.tabletM()} {
     width: 5.3rem;
