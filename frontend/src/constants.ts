757fc9451ac4a076b909067ec17323d4f7fb908f
Soomin Sohn <soomin.sohn.dev@gmail.com>
2022-10-13 18:10:23 +0900

    refactor: button변경

--- frontend/src/constants.ts
+++ frontend/src/constants.ts
@@ -9,7 +9,7 @@ export const PAGE = {
   COACH_INTERVIEW_CREATE: '/coach/interview/create',
   COACH_HOME: '/coach/home',
   MY_PAGE: '/mypage',
-  OAUTH_REDIRECT: '/api/login/crew',
+  OAUTH_REDIRECT: '/login/true',
   ACCESS_DENY: '/access/deny',
   NOT_FOUND: '*',
 } as const;
