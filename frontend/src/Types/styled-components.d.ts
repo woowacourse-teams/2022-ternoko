2362f53abafd35c8a831583fe26bf1505ec6663f
kimsanglok <evertree6031@gmail.com>
2022-10-19 18:35:10 +0900

    refactor: modify styled-components.d.ts

--- frontend/src/Types/styled-components.d.ts
+++ frontend/src/Types/styled-components.d.ts
@@ -1,11 +1,11 @@
-import { Colors, Devices, Shadows } from '@/types/styles/theme';
-
 import 'styled-components';
+
+import theme from '@/Styles/theme';
 
 declare module 'styled-components' {
   export interface DefaultTheme {
-    colors: Colors;
-    shadows: Shadows;
-    devices: Devices;
+    colors: { [K in keyof typeof theme.colors]: typeof theme.colors[K] };
+    shadows: { [K in keyof typeof theme.shadows]: typeof theme.shadows[K] };
+    devices: { [K in keyof typeof theme.devices]: typeof theme.devices[K] };
   }
 }
